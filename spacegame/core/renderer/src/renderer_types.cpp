657fc2622d77acead879da6fd0a55ab758f15870
sefi <sefi@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2006-06-19 20:13:33 +0000

    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@14 469afb1b-a615-0410-ae09-9c7fe5ec7362

---
+++ spacegame/core/renderer/src/renderer_types.cpp
@@ -0,0 +1,11 @@
+#include "../renderer_types.hpp"
+
+const sge::color sge::colors::white;
+const sge::color sge::colors::black;
+const sge::color sge::colors::red;
+const sge::color sge::colors::blue;
+const sge::color sge::colors::green;
+const sge::color sge::colors::yellow;
+const sge::color sge::colors::purple;
+const sge::color sge::colors::orange;
+const sge::color sge::colors::transparent;
