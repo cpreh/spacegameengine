db0b0770737928d6083df27b757488f79b64c6b0
slashlife <slashlife@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2007-06-28 15:20:44 +0000

    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@189 469afb1b-a615-0410-ae09-9c7fe5ec7362

---
+++ src/detail/iconv_encoding_list.hpp
@@ -0,0 +1,27 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006  Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+//                  encoding enum         windows          linux
+SGE_ICONV_ENCODING2( enc_ascii,           ASCII                      ),
+SGE_ICONV_ENCODING3( enc_string_literal,  CP1252,          UTF-8     ),
+SGE_ICONV_ENCODING3( enc_wstring_literal, UTF-16,          UTF-32    ),
+SGE_ICONV_ENCODING2( enc_utf8,            UTF-8                      ),
+SGE_ICONV_ENCODING2( enc_utf16,           UTF-16                     ),
+SGE_ICONV_ENCODING3( enc_ucs_4_internal,  UTF-32LE,        UTF32LE   )
