fc046e3ff849854c2f760126c6549cc1e03590a5
phillemann <phillemann@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2007-09-18 14:24:45 +0000

    -Deleted math.hpp and moved everything to utility.hpp.
    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@384 469afb1b-a615-0410-ae09-9c7fe5ec7362

--- src/math/utility.hpp
+++ src/math/utility.hpp
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <limits>
 #include <boost/utility/enable_if.hpp>
 #include <boost/type_traits/is_floating_point.hpp>
+#include <boost/type_traits/is_integral.hpp>
 
 namespace sge
 {
@@ -70,6 +71,64 @@ template<typename T>
 bool almost_zero(const T t)
 {
 	return compare(t, static_cast<T>(0));
+}
+
+unsigned log2(const unsigned x)
+{
+  unsigned r = 0;
+  while((x >> r) != 0)
+    r++;
+  return --r;
+}
+
+bool is_int_log2(const unsigned x)
+{
+	return !(x & (x-1));
+}
+
+template<typename Base, typename Exp> Base pow_int(const Base base, const Exp e)
+{
+	if(e == 0)
+		return 1;
+	Base ret = base;
+	for(Exp i = 1; i < e; ++i)
+		ret*=base;
+	return ret;
+}
+
+template<typename T>
+typename boost::enable_if<boost::is_integral<T>, T>::type round_div_int(const T  l, const T r)
+{
+	return (r % 2)
+		? ((l * 2 / r) + 1) / 2 // for odd numbers up to half the types capacity
+		: (l + (r / 2)) / r;   // for even numbers at least up to half till up
+		                        // to 100% of the types capacity, depending on r
+		                        // greater r -> less max l
+}
+
+template<bool is_integral> struct next_pow_2_implementation {
+	template<typename T> static T next_pow_2(const T t)
+	{
+		return static_cast<T>(std::pow(static_cast<T>(2),std::ceil(std::log(static_cast<double>(t))/std::log(static_cast<double>(2)))));
+	}
+};
+
+template<> struct next_pow_2_implementation<true> {
+	template<typename T> static T next_pow_2(const T t)
+	{
+		if (t <= 0) std::log(static_cast<double>(-1)); // throw same exception as general template
+		if (t & (t-1)) {
+			register unsigned char c = t & 0x3f;
+			register T t_copy = t, ret=2;
+			while (t_copy >>= 6) { ret <<= 6; c = t_copy & 0x3f; }
+			return ret <<= (c & 0x20) ? 5 : (c & 0x10) ? 4 : (c & 0x08) ? 3 : (c & 0x04) ? 2 : (c & 0x02) ? 1 : 0;
+		} else return t;
+	}
+};
+
+template<typename T> inline T next_pow_2(const T t) {
+	typedef typename boost::is_integral<T> is_int;
+	return next_pow_2_implementation<is_int::value>::next_pow_2(t);
 }
 
 }
