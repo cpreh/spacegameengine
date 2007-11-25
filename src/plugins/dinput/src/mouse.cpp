a7764b18ec4eedace6dceb42e89e2bb3d7f35c56
slashlife <slashlife@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2007-11-25 14:03:54 +0000

    mouse speed adjustment, small fix (bound_pos now has 0 as a lower boundary instead of doing nothing)
    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@477 469afb1b-a615-0410-ae09-9c7fe5ec7362

--- src/plugins/dinput/src/mouse.cpp
+++ src/plugins/dinput/src/mouse.cpp
@@ -29,8 +29,9 @@ inline void bound_pos(sge::key_state& state)
 {
 //	if(state > 1.f)
 //		state = 1.f;
-//	else if(state < 0)
-//		state = 0;
+//	else
+	if(state < 0)
+		state = 0;
 }
 
 inline void bound_pos_neg(sge::key_state& state)
