a77828ddfc90dbb847ffd03c9c37f77486d8e8ff
freundlich <freundlich@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2008-03-29 12:38:16 +0000

    Fixed build errors.
    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@682 469afb1b-a615-0410-ae09-9c7fe5ec7362

--- src/math/src/point_rotate.cpp
+++ src/math/src/point_rotate.cpp
@@ -1,6 +1,7 @@
 #include "../point_rotate.hpp"
+#include "../matrix_impl.hpp"
 
-sge::math::vector2 sge::math::point_rotate(const vector2 &point,const vector2 &around,const space_unit rot);
+sge::math::vector2 sge::math::point_rotate(const vector2 &point,const vector2 &around,const space_unit rot)
 {
   return (matrix_rotation_2d(rot) * (point - around)) + around;
 }
