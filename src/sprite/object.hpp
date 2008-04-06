bb143e0162147bd9ba1f6c1655dbab5ae0cc52ab
freundlich <freundlich@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2008-04-06 13:29:25 +0000

    Added sprite/fwd.hpp.
    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@698 469afb1b-a615-0410-ae09-9c7fe5ec7362

--- src/sprite/object.hpp
+++ src/sprite/object.hpp
@@ -18,8 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef SGE_SPRITE_HPP_INCLUDED
-#define SGE_SPRITE_HPP_INCLUDED
+#ifndef SGE_SPRITE_OBJECT_HPP_INCLUDED
+#define SGE_SPRITE_OBJECT_HPP_INCLUDED
 
 #include "../math/rect.hpp"
 #include "../math/vector.hpp"
