c1ff9bc03c486bb753af419795b87b958ff004a6
freundlich <freundlich@469afb1b-a615-0410-ae09-9c7fe5ec7362>
2008-07-13 12:26:12 +0000

    Simplified global logger.
    git-svn-id: https://spacegameengine.svn.sourceforge.net/svnroot/spacegameengine@799 469afb1b-a615-0410-ae09-9c7fe5ec7362

--- src/log/global.cpp
+++ src/log/global.cpp
@@ -2,37 +2,12 @@
 #include <sge/log/logger.hpp>
 #include <sge/log/level.hpp>
 #include <sge/iostream.hpp>
-#include <sge/once.hpp>
-#include <boost/scoped_ptr.hpp>
-
-namespace
-{
-
-boost::scoped_ptr<
-	sge::log::logger
-> instance;
-
-void init();
-
-}
 
 sge::log::logger &
 sge::log::global()
 {
-	init();
-	return *instance;
-}
-
-namespace
-{
-
-void init()
-{
-	SGE_FUNCTION_ONCE
-	instance.reset(
-		new sge::log::logger(
-			sge::log::level::debug,
-			sge::cerr));
-}
-
+	static logger l(
+		level::debug,
+		sge::cerr);
+	return l;
 }
