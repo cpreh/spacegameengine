c5d84c5beaf6a06f7a8fe8b0645abb5ce02dd2a7
Philipp Reh <sefi@s-e-f-i.de>
2008-08-14 15:36:36 +0200

    Added a missing raw_vector_impl include.

--- src/plugins/opengl/src/readwrite_texture_lock.cpp
+++ src/plugins/opengl/src/readwrite_texture_lock.cpp
@@ -18,6 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 #include "../readwrite_texture_lock.hpp"
+#include <sge/raw_vector_impl.hpp>
 
 sge::ogl::readwrite_texture_lock::readwrite_texture_lock(
 	size_type const lock_size,
