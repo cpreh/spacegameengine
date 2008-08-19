ebcbf7e703ca3bddc6d3ed8dc6439de64f22c6ed
phillemann <pmidden@gmx.net>
2008-08-19 17:56:58 +0200

    added utility functions and the gil image hack to renderer

---
+++ src/renderer/image_view_hack.cpp
@@ -0,0 +1,57 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2007  Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <sge/renderer/image_view_hack.hpp>
+#include <boost/gil/extension/dynamic_image/algorithm.hpp>
+#include <boost/gil/extension/dynamic_image/apply_operation.hpp>
+
+namespace 
+{
+template <typename Dst>
+struct variant_const_assign_fn 
+{
+	typedef void result_type;
+	Dst& _dst;
+	variant_const_assign_fn(Dst& dst) : _dst(dst) {}
+
+	template <typename Src> void operator()(const Src& src) const 
+	{
+		typename Src::const_t const_src(src);
+		_dst = const_src;
+	}
+};
+
+template <typename V1, typename V2>
+void variant_const_assign(
+	boost::gil::variant<V1> const &src, 
+	boost::gil::variant<V2>& dst) 
+{
+	variant_const_assign_fn<boost::gil::variant<V2> > fn(dst);
+	boost::gil::apply_operation(src,fn);
+} 
+}
+
+
+sge::renderer::const_image_view const sge::renderer::make_const_view(
+	image_view const &ncv)
+{
+	const_image_view cv;
+	variant_const_assign(ncv,cv);
+	return cv;
+}
