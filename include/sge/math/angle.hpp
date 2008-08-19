6ec06a12cb0b13cf976adda1f6da2f203fe39f12
phillemann <pmidden@gmx.net>
2008-08-19 21:22:22 +0200

    fixed documentation for angle

--- include/sge/math/angle.hpp
+++ include/sge/math/angle.hpp
@@ -65,21 +65,16 @@ angle_to(
 	return atan2(structure_cast<Dest>(to));
 }
 
-<<<<<<< HEAD:include/sge/math/angle.hpp
 /** 
  * Calculates an angle in the range \f$[-\pi,\pi]\f$ to an absolute angle in
  * \f$[0,2 \pi]\f$
  */
-space_unit rel_angle_to_abs(const space_unit);
+SGE_SYMBOL space_unit rel_angle_to_abs(space_unit);
 /** 
  * Does the inverse of rel_angle_to_abs
  */
-space_unit abs_angle_to_rel(space_unit);
-=======
-SGE_SYMBOL space_unit rel_angle_to_abs(space_unit);
 SGE_SYMBOL space_unit abs_angle_to_rel(space_unit);
 SGE_SYMBOL bool is_rel_angle(space_unit);
->>>>>>> master:include/sge/math/angle.hpp
 
 }
 }
