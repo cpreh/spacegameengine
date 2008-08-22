9d9c41e746b9687287fb89079e9093d0ec22ab13
phillemann <pmidden@gmx.net>
2008-08-22 12:09:24 +0200

    added two asserts

--- include/sge/assert.hpp
+++ include/sge/assert.hpp
@@ -14,7 +14,7 @@ SGE_SYMBOL void process_assert(string const &file,string const &line,string cons
 }
 }
 
-#define SGE_ASSERT(cond,message) if (!(cond)) sge::detail::process_assert(SGE_TEXT(__FILE__),BOOST_PP_STRINGIZE(__LINE__),SGE_TEXT(#cond),message);
+#define SGE_ASSERT_MESSAGE(cond,message) if (!(cond)) sge::detail::process_assert(SGE_TEXT(__FILE__),BOOST_PP_STRINGIZE(__LINE__),SGE_TEXT(#cond),message);
 
 #define SGE_ASSERT(cond) if (!(cond)) sge::detail::process_assert(SGE_TEXT(__FILE__),BOOST_PP_STRINGIZE(__LINE__),SGE_TEXT(#cond),SGE_TEXT(BOOST_PP_STRINGIZE(message)));
 
