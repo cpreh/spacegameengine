f98036d0a40017baedd2fd0136ba656e6d98c62d
Philipp Reh <sefi@s-e-f-i.de>
2008-10-23 20:36:11 +0200

    Export GIL instantiations.

--- src/plugins/opengl/src/target.cpp
+++ src/plugins/opengl/src/target.cpp
@@ -23,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "../framebuffer_functions.hpp"
 #include "../color_convert.hpp"
 #include <sge/renderer/make_image_view.hpp>
-#include <sge/renderer/image_view_impl.hpp>
 #include <sge/math/rect_impl.hpp>
 #include <sge/exception.hpp>
 #include <sge/text.hpp>
--- src/renderer/image_view.cpp
+++ src/renderer/image_view.cpp
@@ -1,5 +1,26 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2007  Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
 #include <sge/renderer/image_view.hpp>
 #include <sge/renderer/image_view_impl.hpp>
+#include <sge/export.hpp>
 
 typedef boost::gil::variant<
 	boost::gil::detail::views_get_const_t<
@@ -11,31 +32,31 @@ typedef boost::gil::variant<
 	sge::renderer::image_view_elements
 > image_view_elements;
 
-template
+template SGE_SYMBOL
 boost::gil::detail::destructor_op::result_type
 boost::gil::apply_operation(
 	const_image_view_elements &,
 	boost::gil::detail::destructor_op);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::equal_to_fn<const_image_view_elements::base_t>::result_type
 boost::gil::apply_operation(
 	const_image_view_elements &,
 	boost::gil::detail::equal_to_fn<const_image_view_elements::base_t>);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::copy_construct_in_place_fn<const_image_view_elements::base_t>::result_type
 boost::gil::apply_operation(
 	const_image_view_elements const &,
 	boost::gil::detail::copy_construct_in_place_fn<const_image_view_elements::base_t>);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::any_type_get_num_channels::result_type
 boost::gil::apply_operation(
 	const_image_view_elements const &,
 	boost::gil::detail::any_type_get_num_channels);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::any_type_get_dimensions::result_type
 boost::gil::apply_operation(
 	const_image_view_elements const &,
@@ -44,31 +65,31 @@ boost::gil::apply_operation(
 
 
 
-template
+template SGE_SYMBOL
 boost::gil::detail::destructor_op::result_type
 boost::gil::apply_operation(
 	image_view_elements &,
 	boost::gil::detail::destructor_op);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::equal_to_fn<image_view_elements::base_t>::result_type
 boost::gil::apply_operation(
 	image_view_elements &,
 	boost::gil::detail::equal_to_fn<image_view_elements::base_t>);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::copy_construct_in_place_fn<image_view_elements::base_t>::result_type
 boost::gil::apply_operation(
 	image_view_elements const &,
 	boost::gil::detail::copy_construct_in_place_fn<image_view_elements::base_t>);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::any_type_get_num_channels::result_type
 boost::gil::apply_operation(
 	image_view_elements const &,
 	boost::gil::detail::any_type_get_num_channels);
 
-template
+template SGE_SYMBOL
 boost::gil::detail::any_type_get_dimensions::result_type
 boost::gil::apply_operation(
 	image_view_elements const &,
