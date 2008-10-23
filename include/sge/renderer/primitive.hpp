de9855322d9bb81335ebaa130097e0964de9355b
Philipp Reh <sefi@s-e-f-i.de>
2008-10-23 21:43:30 +0200

    Removed some unnecessary includes

--- include/sge/renderer/primitive.hpp
+++ include/sge/renderer/primitive.hpp
@@ -21,9 +21,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef SGE_RENDERER_PRIMITIVE_HPP_INCLUDED
 #define SGE_RENDERER_PRIMITIVE_HPP_INCLUDED
 
-#include "size_type.hpp"
-#include "../export.hpp"
-
 namespace sge
 {
 namespace renderer
