8e2448e30862485a471f54f3845d35280cc6771b
Philipp Reh <sefi@s-e-f-i.de>
2008-11-28 22:02:14 +0100

    Added a missing include of raw_vector_impl for gcc-4.4.

--- src/plugins/mad/src/loader.cpp
+++ src/plugins/mad/src/loader.cpp
@@ -24,6 +24,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/log/headers.hpp>
 #include <sge/text.hpp>
 
+#include <sge/raw_vector_impl.hpp>
+
 sge::audio::file_ptr const
 sge::mad::loader::load(
 	path const &filename)
