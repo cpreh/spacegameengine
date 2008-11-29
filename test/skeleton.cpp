7823fd6817c6f343589e2f5eee392d8630ce7345
Philipp Reh <sefi@s-e-f-i.de>
2008-11-29 20:43:28 +0100

    Added a private to skeleton test.

--- test/skeleton.cpp
+++ test/skeleton.cpp
@@ -34,6 +34,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 struct app : public sge::mainloop::skeleton {
 	app();
+private:
 	void loop();
 	void key_event(
 		sge::input::key_pair const &);
