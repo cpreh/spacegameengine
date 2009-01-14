3880c33009bc4938256da4f6b0900d1026a0fcdb
phillemann <pmidden@gmx.net>
2009-01-14 17:17:35 +0100

    fixed modifier compilation

--- include/sge/input/modifier/filter.hpp
+++ include/sge/input/modifier/filter.hpp
@@ -2,6 +2,7 @@
 #define SGE_INPUT_MODIFIER_FILTER_HPP_INCLUDED
 
 #include "states.hpp"
+#include "../../export.hpp"
 #include "../key_pair_fwd.hpp"
 #include "../system_fwd.hpp"
 #include "../../signals/scoped_connection.hpp"
@@ -14,15 +15,15 @@ namespace input
 {
 namespace modifier
 {
-class filter
+class SGE_CLASS_SYMBOL filter
 {
 	public:
 	typedef void fn_callback_type (key_pair const &,states const &);
 	typedef boost::function<fn_callback_type> callback_type;
 
-	explicit filter(system_ptr);
+	SGE_SYMBOL explicit filter(system_ptr);
 
-	signals::scoped_connection const register_callback(
+	SGE_SYMBOL signals::scoped_connection const register_callback(
 		callback_type const &);
 	private:
 	signals::signal<fn_callback_type> signal;
