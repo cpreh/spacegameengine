6e6814c7cb11c60cd73561b78e3c1d24ba797a11
Philipp Reh <sefi@s-e-f-i.de>
2009-01-31 13:42:11 +0100

    Some updates for glsl.

--- src/plugins/opengl/glsl/src/uniform_setter.cpp
+++ src/plugins/opengl/glsl/src/uniform_setter.cpp
@@ -28,6 +28,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/math/vector/dynamic.hpp>
 #include <sge/array_wrapper_impl.hpp>
 #include <sge/once.hpp>
+#include <sge/exception.hpp>
+#include <sge/text.hpp>
+#include <sge/format.hpp>
+#include <sge/assert.hpp>
 #include <sge/raw_vector_impl.hpp>
 #include <boost/variant/apply_visitor.hpp>
 #include <boost/variant/static_visitor.hpp>
@@ -66,37 +70,68 @@ convert_array(
 	sge::array_wrapper<T> const &);
 
 struct vector_visitor
-:boost::static_visitor<sge::ogl::glsl::uniform_type> {
+: boost::static_visitor<sge::ogl::glsl::uniform_type> {
 
 	explicit vector_visitor(
 		GLint location);
 	
 	template<
+		typename T,
 		typename N,
 		typename S
 	>
 	sge::ogl::glsl::uniform_type const
 	operator()(
 		sge::math::vector::basic<
-			GLfloat,
-			N,
-			S
-		> const &) const;
-
-	template<
-		typename N,
-		typename S
-	>
-	sge::ogl::glsl::uniform_type const
-	operator()(
-		sge::math::vector::basic<
-			GLint,
+			T,
 			N,
 			S
 		> const &) const;
 private:
 	GLint const location;
 };
+
+struct vector_array_visitor
+: boost::static_visitor<sge::ogl::glsl::uniform_type> {
+
+	typedef sge::array_wrapper<
+		sge::renderer::glsl::uniform_vector
+	> array_type;
+
+	vector_array_visitor(
+		GLint location,
+		array_type const &);
+	
+	template<
+		typename T,
+		typename N,
+		typename S
+	>
+	sge::ogl::glsl::uniform_type const
+	operator()(
+		sge::math::vector::basic<
+			T,
+			N,
+			S
+		> const &) const;
+private:
+	GLint const location;
+	array_type const &arr;
+};
+
+sge::ogl::glsl::uniform_type const
+set_vector(
+	GLint location,
+	GLfloat const *data,
+	GLsizei size,
+	GLsizei elements);
+
+sge::ogl::glsl::uniform_type const
+set_vector(
+	GLint location,
+	GLint const *data,
+	GLsizei size,
+	GLsizei elements);
 
 sge::ogl::glsl::uniform_type const
 set_matrix(
@@ -150,31 +185,6 @@ sge::ogl::glsl::uniform_setter::operator()(
 		vector_visitor(
 			location),
 		v);
-
-/*
-	switch(v.size()) {
-	case 2:
-		uniform_2fv(
-			location,
-			1u,
-			v.data());
-		return uniform_type::float2;
-	case 3:
-		uniform_3fv(
-			location,
-			1u,
-			v.data());
-		return uniform_type::float3;
-	case 4:
-		uniform_4fv(
-			location,
-			1u,
-			v.data());
-		return uniform_type::float4;
-	default:
-		throw exception(
-			SGE_TEXT("Invalid vector size in glsl uniform variable."));
-	}*/
 }
 
 sge::ogl::glsl::uniform_type const
@@ -187,13 +197,6 @@ sge::ogl::glsl::uniform_setter::operator()(
 		m.rows(),
 		m.columns(),
 		1);
-	/*
-	uniform_matrix_4fv(
-		location,
-		1u,
-		false,
-		m.data());
-	return uniform_type::matrix4x4;*/
 }
 
 sge::ogl::glsl::uniform_type const
@@ -228,24 +231,21 @@ sge::ogl::glsl::uniform_type const
 sge::ogl::glsl::uniform_setter::operator()(
 	array_wrapper<renderer::glsl::uniform_vector> const &a) const
 {
-	/*
-	uniform_2fv(
-		location,
-		static_cast<GLsizei>(a.size()),
-		convert_array<
-			GLfloat
-		>(a).data());
-	return uniform_type::float2_array;*/
+	SGE_ASSERT(!a.empty());
+
+	return boost::apply_visitor(
+		vector_array_visitor(
+			location,
+			a),
+		*a.begin());
 }
 
 sge::ogl::glsl::uniform_type const
 sge::ogl::glsl::uniform_setter::operator()(
 	array_wrapper<renderer::glsl::uniform_matrix> const &a) const
 {
-	// TODO: check if the array is empty
-	// sanity check all the elements
-	
-
+	// TODO: sanity check all the elements
+	SGE_ASSERT(!a.empty());	
 
 	return set_matrix(
 		location,
@@ -255,16 +255,6 @@ sge::ogl::glsl::uniform_setter::operator()(
 		a.begin()->rows(),
 		a.begin()->columns(),
 		a.size());
-
-	/*
-	uniform_matrix_4fv(
-		location,
-		static_cast<GLsizei>(a.size()),
-		false,
-		convert_array<
-			GLfloat
-		>(a).data());
-	return uniform_type::matrix4x4_array;*/
 }
 
 namespace
@@ -310,13 +300,6 @@ void initialize_uniform_setter()
 		uniform_matrix_2fv = glUniformMatrix2fvARB;
 		uniform_matrix_3fv = glUniformMatrix3fvARB;
 		uniform_matrix_4fv = glUniformMatrix4fvARB;
-		/*
-		uniform_matrix_2x3fv = glUniformMatrix2x3fvARB;
-		uniform_matrix_3x2fv = glUniformMatrix3x2fvARB;
-		uniform_matrix_2x4fv = glUniformMatrix2x4fvARB;
-		uniform_matrix_4x2fv = glUniformMatrix4x2fvARB;
-		uniform_matrix_3x4fv = glUniformMatrix3x4fvARB;
-		uniform_matrix_4x3fv = glUniformMatrix4x3fvARB;*/
 	}
 }
 
@@ -341,33 +324,144 @@ vector_visitor::vector_visitor(
 {}
 	
 template<
+	typename T,
 	typename N,
 	typename S
 >
 sge::ogl::glsl::uniform_type const
 vector_visitor::operator()(
 	sge::math::vector::basic<
-		GLfloat,
+		T,
 		N,
 		S
-	> const &) const
+	> const &v) const
 {
-
+	return set_vector(
+		location,
+		v.data(),
+		v.size(),
+		1);
 }
 
+
+vector_array_visitor::vector_array_visitor(
+	GLint const location,
+	array_type const &arr)
+:
+	location(location),
+	arr(arr)
+{}
+	
 template<
+	typename T,
 	typename N,
 	typename S
 >
 sge::ogl::glsl::uniform_type const
-vector_visitor::operator()(
+vector_array_visitor::operator()(
 	sge::math::vector::basic<
-		GLint,
+		T,
 		N,
 		S
 	> const &) const
 {
-	
+	/*
+	sge::raw_vector<
+		T
+	> narray;
+
+	for(typename sge::array_wrapper<T>::iterator it(a.begin()); it != a.end(); ++it)
+		ret.insert(ret.end(), it->begin(), it->end());*/
+}
+
+sge::ogl::glsl::uniform_type const
+set_vector(
+	GLint const location,
+	GLfloat const *const data,
+	GLsizei const size,
+	GLsizei const elements)
+{
+	namespace et = sge::ogl::glsl::uniform_element_type;
+
+	switch(size) {
+	case 2:
+		uniform_2fv(
+			location,
+			elements,
+			data);
+
+		return sge::ogl::glsl::uniform_type(
+			et::float2,
+			elements);
+	case 3:
+		uniform_3fv(
+			location,
+			elements,
+			data);
+
+		return sge::ogl::glsl::uniform_type(
+			et::float3,
+			elements);
+	case 4:
+		uniform_4fv(
+			location,
+			elements,
+			data);
+
+		return sge::ogl::glsl::uniform_type(
+			et::float4,
+			elements);
+	default:
+		throw sge::exception(
+			(sge::format(
+				SGE_TEXT("Invalid vector dimension in glsl: %1%"))
+			% size).str());
+	}
+}
+
+sge::ogl::glsl::uniform_type const
+set_vector(
+	GLint const location,
+	GLint const *const data,
+	GLsizei const size,
+	GLsizei const elements)
+{
+	namespace et = sge::ogl::glsl::uniform_element_type;
+
+	switch(size) {
+	case 2:
+		uniform_2iv(
+			location,
+			elements,
+			data);
+
+		return sge::ogl::glsl::uniform_type(
+			et::int2,
+			elements);
+	case 3:
+		uniform_3iv(
+			location,
+			elements,
+			data);
+
+		return sge::ogl::glsl::uniform_type(
+			et::int3,
+			elements);
+	case 4:
+		uniform_4iv(
+			location,
+			elements,
+			data);
+
+		return sge::ogl::glsl::uniform_type(
+			et::int4,
+			elements);
+	default:
+		throw sge::exception(
+			(sge::format(
+				SGE_TEXT("Invalid vector dimension in glsl: %1%"))
+			% size).str());
+	}
 }
 
 sge::ogl::glsl::uniform_type const
@@ -378,6 +472,19 @@ set_matrix(
 	GLsizei const columns,
 	GLsizei const elements)
 {
+	if(
+		!sge::ogl::glsl::is_native()
+		&& rows != columns
+	)
+		throw sge::exception(
+			(sge::format(
+				SGE_TEXT("You specified a glsl matrix whose dimensions are not equal which is not supported by the ARB extension!")
+				SGE_TEXT(" Specified size was: %1%x%2%"))
+			% columns
+			% rows).str());
+
+	namespace et = sge::ogl::glsl::uniform_element_type;
+
 	switch(rows) {
 	case 2:
 		switch(columns) {
@@ -389,15 +496,105 @@ set_matrix(
 				data);
 
 			return sge::ogl::glsl::uniform_type(
-				sge::ogl::glsl::uniform_element_type::matrix2x2,
+				et::matrix2x2,
 				elements);
-		default:
-			break;
+		case 3:
+			uniform_matrix_3x2fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix3x2,
+				elements);
+		case 4:
+			uniform_matrix_4x2fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix4x2,
+				elements);
 		}
-	default:
+		break;
+	case 3:
+		switch(columns) {
+		case 2:
+			uniform_matrix_3x2fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix3x2,
+				elements);
+		case 3:
+			uniform_matrix_3fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix3x3,
+				elements);
+		case 4:
+			uniform_matrix_4x3fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix4x3,
+				elements);
+		}
+		break;
+	case 4:
+		switch(columns) {
+		case 2:
+			uniform_matrix_4x2fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix4x2,
+				elements);
+		case 3:
+			uniform_matrix_3x4fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix3x4,
+				elements);
+		case 4:
+			uniform_matrix_4fv(
+				location,
+				elements,
+				false,
+				data);
+
+			return sge::ogl::glsl::uniform_type(
+				et::matrix4x4,
+				elements);
+		}
 		break;
 	}
-	//
+
+	throw sge::exception(
+		(sge::format(
+			SGE_TEXT("Invalid matrix dimensions in glsl: %1%x%2%"))
+			% columns
+			% rows).str());
 }
 
 }
