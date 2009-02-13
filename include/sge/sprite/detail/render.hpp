1b71fe24c3d74a166ceccb99326cec6ae47e6d96
Philipp Reh <sefi@s-e-f-i.de>
2009-02-13 16:43:22 +0100

    Changed sprite::render.

--- include/sge/sprite/detail/render.hpp
+++ include/sge/sprite/detail/render.hpp
@@ -21,13 +21,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef SGE_SPRITE_DETAIL_RENDER_HPP_INCLUDED
 #define SGE_SPRITE_DETAIL_RENDER_HPP_INCLUDED
 
-#include "constants.hpp"
-#include "../../renderer/size_type.hpp"
-#include "../../renderer/device.hpp"
-#include "../../renderer/texture.hpp"
-#include "../../texture/part.hpp"
-#include "../../algorithm/first_mismatch.hpp"
-#include <iterator>
+#include <sge/sprite/detail/constants.hpp>
+#include <sge/renderer/size_type.hpp>
+#include <sge/renderer/device.hpp>
+#include <sge/renderer/texture.hpp>
+#include <sge/texture/part.hpp>
 
 namespace sge
 {
@@ -36,7 +34,10 @@ namespace sprite
 namespace detail
 {
 
-template<typename In, typename Comp>
+template<
+	typename In,
+	typename Comp
+>
 void render(
 	In const beg,
 	In const end,
@@ -45,15 +46,16 @@ void render(
 	renderer::vertex_buffer_ptr const vb,
 	renderer::index_buffer_ptr const ib)
 {
-	renderer::size_type first_index = 0;
+	renderer::size_type offset(0);
 	for(In cur(beg), next(cur); cur != end; cur = next )
 	{
-		renderer::size_type num_objects;
-		next = first_mismatch_if(
-			cur,
-			end,
-			num_objects,
-			comp);
+		renderer::size_type num_objects(0);
+
+		while(next != end && comp(*cur, *next))
+		{
+			++next;
+			++num_objects;
+		}
 
 		if(!cur->visible())
 			continue;
@@ -69,13 +71,13 @@ void render(
 		rend->render(
 			vb,
 			ib,
-			std::distance(beg, cur) * detail::vertices_per_sprite,
-			std::distance(cur, next) * detail::vertices_per_sprite,
+			offset * detail::vertices_per_sprite,
+			num_objects * detail::vertices_per_sprite,
 			renderer::indexed_primitive_type::triangle,
 			num_objects * 2,
-			first_index);
+			offset * detail::indices_per_sprite);
 
-		first_index += num_objects * detail::indices_per_sprite;
+		offset += num_objects;
 	}
 
 	rend->texture(renderer::device::no_texture);
