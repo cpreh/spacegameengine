09f7f4022b90dcd06735009f8baf47e7777450ec
Philipp Reh <sefi@s-e-f-i.de>
2009-03-19 14:44:12 +0100

    Added parse_file function for ini.

---
+++ src/parse/ini/parse_file.cpp
@@ -0,0 +1,71 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2007  Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include <sge/parse/ini/parse_file.hpp>
+#include <sge/parse/ini/parse.hpp>
+#include <sge/fstream.hpp>
+#include <sge/exception.hpp>
+#include <sge/text.hpp>
+#include <sge/string.hpp>
+
+bool
+sge::parse::ini::parse_file(
+	filesystem::path const &path,
+	section_vector &result)
+{
+	// TODO: this should be generalized for all parsers
+	// but currently there is only the ini parser
+	
+	ifstream ifs(
+		path,
+		std::ios_base::binary
+	);
+
+	if(!ifs.is_open())
+		throw exception(
+			SGE_TEXT("Opening ")
+			+ path.string()
+			+ SGE_TEXT(" failed!")
+		);
+	
+	string ret;
+
+	{
+		ifstream::char_type ch;
+		while(ifs.get(ch))
+			ret.push_back(ch);
+	}
+	
+	typedef string::const_iterator iterator;
+
+	iterator beg(
+		ret.begin()
+	);
+
+	return parse(
+		beg,
+		static_cast<
+			string const &
+		>(
+			ret
+		).end(),
+		result
+	);
+}
