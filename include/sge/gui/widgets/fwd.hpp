56038cd571c16e92db45ed781145a1340a4e5a37
Philipp Reh <sefi@s-e-f-i.de>
2009-03-20 13:24:17 +0100

    Fixed compilation.

--- include/sge/gui/widgets/fwd.hpp
+++ include/sge/gui/widgets/fwd.hpp
@@ -20,19 +20,19 @@ class text;
 class image;
 }
 
-#define SGE_WIDGETS_APPLY(begin_func,func,end_func)\
-	begin_func(sge::gui::widgets::edit)\
-	func(sge::gui::widgets::label)\
-	func(sge::gui::widgets::graphics)\
-	func(sge::gui::widgets::backdrop)\
-	func(sge::gui::widgets::buttons::text)\
-	end_func(sge::gui::widgets::buttons::image)
+#define SGE_GUI_WIDGETS_FWD_APPLY(begin_func,func,end_func)\
+	begin_func(edit)\
+	func(label)\
+	func(graphics)\
+	func(backdrop)\
+	func(buttons::text)\
+	end_func(buttons::image)
 
-#define SGE_WIDGETS_FWD_COMMA_WIDGET(w) w,
-#define SGE_WIDGETS_FWD_NO_COMMA_WIDGET(w) w
+#define SGE_GUI_WIDGETS_FWD_COMMA_WIDGET(w) w,
+#define SGE_GUI_WIDGETS_FWD_NO_COMMA_WIDGET(w) w
 
 typedef boost::mpl::vector<
-	SGE_WIDGETS_APPLY(SGE_WIDGETS_FWD_COMMA_WIDGET,SGE_WIDGETS_FWD_COMMA_WIDGET,SGE_WIDGETS_FWD_NO_COMMA_WIDGET)
+	SGE_GUI_WIDGETS_FWD_APPLY(SGE_GUI_WIDGETS_FWD_COMMA_WIDGET,SGE_GUI_WIDGETS_FWD_COMMA_WIDGET,SGE_GUI_WIDGETS_FWD_NO_COMMA_WIDGET)
 > types;
 }
 }
