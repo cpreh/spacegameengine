8382289af380e328c85e415004e159c5bd61b2ad
phillemann <pmidden@gmx.net>
2009-05-17 19:11:24 +0200

    added better fragment shader

--- media/shaders/fragment.glsl
+++ media/shaders/fragment.glsl
@@ -2,31 +2,41 @@ uniform sampler2D tex;
 
 void main()
 {
-   vec4 sum = vec4(0);
-   vec2 texcoord = vec2(gl_TexCoord[0]);
-   int j;
-   int i;
+	vec4 sum = vec4(0);
+	vec2 texcoord = vec2(gl_TexCoord[0]);
 
-   for( i= -4 ;i < 4; i++)
-   {
-        for (j = -3; j < 3; j++)
-        {
-            sum += texture2D(tex, texcoord + vec2(j, i)*0.004) * 0.25;
-        }
-   }
-       if (texture2D(tex, texcoord).r < 0.3)
-    {
-       gl_FragColor = sum*sum*0.012 + texture2D(tex, texcoord);
-    }
-    else
-    {
-        if (texture2D(tex, texcoord).r < 0.5)
-        {
-            gl_FragColor = sum*sum*0.009 + texture2D(tex, texcoord);
-        }
-        else
-        {
-            gl_FragColor = sum*sum*0.0075 + texture2D(tex, texcoord);
-        }
-    }
+	const int box = 4;
+
+	for(int x = -box; x < box; x++)
+	{
+		for (int y = -box; y < box; y++)
+		{
+			vec4 current = texture2D(
+				tex,
+				vec2(
+					texcoord.x+x/1024.0,
+					texcoord.y+y/768.0));
+
+			// contrast stuff begin
+			/*
+			const float coeff = 0.8;
+			for (int i = 0; i < 4; ++i)
+			{
+				float ex = -((current[i]*16.0)-8.0)/coeff;
+				current[i] = (1.0/(1.0+exp(ex)));
+			}
+			*/
+			for (int i = 0; i < 4; ++i)
+			{
+				current[i] = 3.0*current[i]*current[i]-2.0*current[i]*current[i]*current[i];
+			}
+			// contrast stuff end
+			
+			sum += current/float(4*box*box);
+		}
+	}
+
+	gl_FragColor = sum + texture2D(
+		tex,
+		texcoord);
 }
