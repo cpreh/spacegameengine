dc2a9f61b43e83bd35361343dedf4e22748ca2ee
Philipp Reh <sefi@s-e-f-i.de>
2009-05-21 12:44:47 +0200

    Split conversion functions in d3d9.

---
+++ src/plugins/d3d9/src/convert_resource_flags.cpp
@@ -0,0 +1,33 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2009 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include "../convert_resource_flags.hpp"
+
+DWORD
+sge::d3d9::convert_resource_flags(
+	renderer::resource_flag_t const r)
+{
+	DWORD l = 0;
+	if(r & renderer::resource_flags::dynamic)
+		l |= D3DUSAGE_DYNAMIC;
+	if(r & renderer::resource_flags::write_only)
+		l |= D3DUSAGE_WRITEONLY;
+	return l;
+}
---
+++ src/plugins/d3d9/src/convert_to_pool.cpp
@@ -0,0 +1,30 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2009 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include "../convert_to_pool.hpp"
+
+D3DPOOL
+sge::d3d9::convert_to_pool(
+	renderer::resource_flag_t const r)
+{
+	return r & renderer::resource_flags::dynamic
+		? D3DPOOL_DEFAULT
+		: D3DPOOL_MANAGED;
+}
