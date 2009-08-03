e448dcbc362b7c3ed9bc5bb9a4c2d5db7a79c8b5
phillemann <pmidden@gmx.net>
2009-08-03 20:53:56 +0200

    image/loadear.hpp should include loader_fwd

--- include/sge/image/loader.hpp
+++ include/sge/image/loader.hpp
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/image/file_fwd.hpp>
 #include <sge/image/view/const_object.hpp>
 //#include <sge/image/format.hpp>
+#include <sge/image/loader_fwd.hpp>
 #include <sge/filesystem/path.hpp>
 #include <sge/export.hpp>
 #include <sge/noncopyable.hpp>
