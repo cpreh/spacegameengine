5206f7ca958e0910b8ac014303e1a3e3ba74f407
Philipp Reh <sefi@s-e-f-i.de>
2009-12-12 21:19:09 +0100

    Changed assign_pre.

--- include/sge/sprite/detail/assign_pre.hpp
+++ include/sge/sprite/detail/assign_pre.hpp
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define SGE_SPRITE_DETAIL_ASSIGN_PRE_HPP_INCLUDED
 
 #include <sge/sprite/intrusive/detail/object_base_hook.hpp>
-#include <sge/sprite/object_fwd.hpp>
+#include <sge/sprite/object_decl.hpp>
 #include <boost/mpl/contains.hpp>
 #include <boost/utility/enable_if.hpp>
 
@@ -55,9 +55,9 @@ assign_pre(
 	this_.unlink();
 
 	static_cast<
-		intrusive::detail::object_base_hook
+		intrusive::detail::object_base_hook &
 	>(
-		&this_
+		this_
 	) = other_;
 }
 
--- include/sge/sprite/intrusive/adder_impl.hpp
+++ include/sge/sprite/intrusive/adder_impl.hpp
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define SGE_SPRITE_INTRUSIVE_ADDER_IMPL_HPP_INCLUDED
 
 #include <sge/sprite/intrusive/adder_decl.hpp>
+#include <sge/sprite/object_decl.hpp>
 
 template<
 	typename Choices
