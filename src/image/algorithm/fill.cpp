fdc29e8d2aa2aefed104d9ad49cad498436346c7
Philipp Reh <sefi@s-e-f-i.de>
2009-12-24 16:20:06 +0100

    Fixed license headers in src.

--- src/image/algorithm/fill.cpp
+++ src/image/algorithm/fill.cpp
@@ -1,6 +1,7 @@
 /*
 spacegameengine is a portable easy to use game engine written in C++.
 Copyright (C) 2006-2009 Carl Philipp Reh (sefi@s-e-f-i.de)
+
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public License
 as published by the Free Software Foundation; either version 2
