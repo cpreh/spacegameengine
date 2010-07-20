bb8f4e7a7f2feb2e1153f48789d281a32dedf549
Philipp Reh <sefi@s-e-f-i.de>
2010-07-20 23:06:30 +0200

    Fixed include guards.

--- src/plugins/opengl/tex_envf_ext.hpp
+++ src/plugins/opengl/tex_envf_ext.hpp
@@ -18,8 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef SGE_OPENGL_TEX_EVNF_EXT_HPP_INCLUDED
-#define SGE_OPENGL_TEX_EVNF_EXT_HPP_INCLUDED
+#ifndef SGE_OPENGL_TEX_ENVF_EXT_HPP_INCLUDED
+#define SGE_OPENGL_TEX_ENVF_EXT_HPP_INCLUDED
 
 #include "common.hpp"
 
