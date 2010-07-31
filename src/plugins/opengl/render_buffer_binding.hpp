2e67c6b137e0524d7f4f943f95a71316fa232afb
Philipp Reh <sefi@s-e-f-i.de>
2010-08-01 01:10:28 +0200

    Fixed include guards.

--- src/plugins/opengl/render_buffer_binding.hpp
+++ src/plugins/opengl/render_buffer_binding.hpp
@@ -18,8 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef SGE_OPENGL_RENDER__BUFFER_BINDING_HPP_INCLUDED
-#define SGE_OPENGL_RENDER__BUFFER_BINDING_HPP_INCLUDED
+#ifndef SGE_OPENGL_RENDER_BUFFER_BINDING_HPP_INCLUDED
+#define SGE_OPENGL_RENDER_BUFFER_BINDING_HPP_INCLUDED
 
 #include "render_buffer_binding_fwd.hpp"
 #include "fbo_context_fwd.hpp"
