6f5b9f32891ebeff13b4d5d6ede912d4a9e471a3
Philipp Reh <sefi@s-e-f-i.de>
2010-11-22 19:13:25 +0100

    Changed some function/object_fwd to function/object.

--- include/sge/input/mouse/button_callback.hpp
+++ include/sge/input/mouse/button_callback.hpp
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define SGE_INPUT_MOUSE_BUTTON_CALLBACK_HPP_INCLUDED
 
 #include <sge/input/mouse/button_function.hpp>
-#include <fcppt/function/object_fwd.hpp>
+#include <fcppt/function/object.hpp>
 
 namespace sge
 {
