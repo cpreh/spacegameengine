e88e1b7d0a61210c8423acaba34ec47897fce0c6
Philipp Reh <sefi@s-e-f-i.de>
2010-12-04 12:09:21 +0100

    Added some cursor types.

---
+++ include/sge/input/cursor/button_event.hpp
@@ -0,0 +1,61 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2010 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef SGE_INPUT_CURSOR_BUTTON_EVENT_HPP_INCLUDED
+#define SGE_INPUT_CURSOR_BUTTON_EVENT_HPP_INCLUDED
+
+#include <sge/input/cursor/button_event_fwd.hpp>
+#include <sge/input/cursor/button_code.hpp>
+#include <sge/symbol.hpp>
+
+namespace sge
+{
+namespace input
+{
+namespace cursor
+{
+
+class button_event
+{
+public:
+	SGE_SYMBOL
+	button_event(
+		cursor::button_code::type,
+		bool pressed
+	);
+
+	SGE_SYMBOL
+	cursor::button_code::type
+	button_code() const;
+
+	SGE_SYMBOL
+	bool
+	pressed() const;
+private:
+	cursor::button_code::type button_code_;
+
+	bool pressed_;
+};
+
+}
+}
+}
+
+#endif
---
+++ include/sge/input/cursor/object.hpp
@@ -0,0 +1,68 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2010 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef SGE_INPUT_CURSOR_OBJECT_HPP_INCLUDED
+#define SGE_INPUT_CURSOR_OBJECT_HPP_INCLUDED
+
+#include <sge/input/cursor/object_fwd.hpp>
+#include <sge/input/cursor/button_callback.hpp>
+#include <sge/input/cursor/move_callback.hpp>
+#include <sge/input/cursor/position.hpp>
+#include <sge/class_symbol.hpp>
+#include <sge/symbol.hpp>
+#include <fcppt/signal/auto_connection.hpp>
+#include <fcppt/noncopyable.hpp>
+
+namespace sge
+{
+namespace input
+{
+namespace cursor
+{
+
+class SGE_CLASS_SYMBOL object
+{
+	FCPPT_NONCOPYABLE(
+		object
+	)
+protected:
+	SGE_SYMBOL object();
+public:
+	SGE_SYMBOL virtual ~object();
+
+	virtual fcppt::signal::auto_connection
+	button_callback(
+		cursor::button_callback const &
+	) = 0;
+
+	virtual fcppt::signal::auto_connection
+	move_callback(
+		cursor::move_callback const &
+	) = 0;
+
+	virtual cursor::position const
+	position() const = 0;
+};
+
+}
+}
+}
+
+#endif
