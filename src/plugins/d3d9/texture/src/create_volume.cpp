513c3dbd8eeb6369878d7d2e4bfa5c82439b4dae
Philipp Reh <sefi@s-e-f-i.de>
2011-03-04 19:11:57 +0100

    More work on d3d9 textures.

---
+++ src/plugins/d3d9/texture/src/create_volume.cpp
@@ -0,0 +1,76 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2011 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include "../create_volume.hpp"
+#include "../mipmap_levels.hpp"
+#include "../../convert/color_format.hpp"
+#include <sge/renderer/texture/volume_parameters.hpp>
+#include <sge/renderer/exception.hpp>
+#include <fcppt/text.hpp>
+
+sge::d3d9::texture::d3d_volume_texture_unique_ptr
+sge::d3d9::texture::create_volume(
+	IDirect3DDevice9 * const _device,
+	renderer::texture::volume_parameters const &_params,
+	D3DPOOL const _pool,
+	d3d9::usage const _usage
+)
+{
+	IDirect3DVolumeTexture9 *ret = 0;
+
+	if(
+		_device->CreateVolumeTexture(
+			static_cast<
+				UINT
+			>(
+				_params.dim().w()
+			),
+			static_cast<
+				UINT
+			>(
+				_params.dim().h()
+			),
+			static_cast<
+				UINT
+			>(
+				_params.dim().d()
+			),
+			texture::mipmap_levels(
+				_params.filter().min()
+			),
+			_usage.get(),
+			d3d9::convert::color_format(
+				_params.color_format()
+			),
+			_pool,
+			&ret,
+			0
+		)
+		!= D3D_OK
+	)
+		throw sge::renderer::exception(
+			FCPPT_TEXT("CreateTexture() failed!")
+		);
+
+	return
+		texture::d3d_volume_texture_unique_ptr(
+			ret
+		);
+}
