80254361e6708b64bb8c4c33f1417baafc6d66d0
phillemann <pmidden@gmx.net>
2011-04-28 22:00:22 +0200

    Removed unnecessary include in line_drawer vf

--- src/line_drawer/vf/color.hpp
+++ src/line_drawer/vf/color.hpp
@@ -22,7 +22,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define SGE_LINE_DRAWER_VF_COLOR_HPP_INCLUDED
 
 #include <sge/line_drawer/color_format.hpp>
-#include <sge/renderer/scalar.hpp>
 #include <sge/renderer/vf/color.hpp>
 
 namespace sge
