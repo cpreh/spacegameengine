e1b7f66ab5fca942f16b163b96c9312541dbb422
Philipp Reh <sefi@s-e-f-i.de>
2011-05-22 13:20:08 +0200

    Comment the stencil example.

--- examples/stencil.cpp
+++ examples/stencil.cpp
@@ -18,6 +18,25 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
+/**
+ 	Example descripton:
+
+	This example demonstrates a simple use case of the stencil buffer.
+	Suppose you have a smaller sprite and a bigger sprite.
+	The smaller sprite is rendered first and the bigger sprite is
+	rendered second. Because the bigger sprite completely overlaps
+	the smaller sprite, only the bigger sprite would be visible under
+	normal circumstances.
+	However, in this example, the stencil buffer is initialized to
+	all 0s on each frame. When the smaller sprite is rendered,
+	the stencil buffer is incremented to 1 for every pixel that
+	the smaller sprite coveres. When the bigger sprite is rendered,
+	the stencil test then compares to the reference value 0,
+	not touching the area in which the smaller sprite was rendered.
+	Thus we create a "hole" in the bigger sprite, leading to the
+	same effect as if the two sprites were rendered in reverse order.
+*/
+
 #include <sge/config/media_path.hpp>
 #include <sge/image/capabilities_field.hpp>
 #include <sge/image/colors.hpp>
@@ -85,7 +104,7 @@ int
 main()
 try
 {
-
+	// The desired window size.
 	sge::window::dim const window_dim(
 		1024,
 		768
@@ -94,6 +113,7 @@ try
 	sge::systems::instance sys(
 		sge::systems::list()
 		(
+			// Request a window with the desired size.
 			sge::systems::window(
 				sge::window::simple_parameters(
 					FCPPT_TEXT("sge stencil example"),
@@ -102,6 +122,9 @@ try
 			)
 		)
 		(
+			// Request a depth and stencil buffer (d24s8).
+			// We don't use the depth buffer, but to be most portable,
+			// d24s8 is the only sensible choice.
 			sge::systems::renderer(
 				sge::renderer::parameters(
 					sge::renderer::visual_depth::depth32,
@@ -109,20 +132,26 @@ try
 					sge::renderer::vsync::on,
 					sge::renderer::no_multi_sampling
 				),
+				// Put the viewport in the center of the window
+				// when the window is bigger than the desired size.
 				sge::viewport::center_on_resize(
 					window_dim
 				)
 			)
 		)
 		(
+			// Request a keyboard collector which acts as if
+			// all the connected keyboards were a sinlge keyboard.
 			sge::systems::input(
 				sge::systems::input_helper_field(
 					sge::systems::input_helper::keyboard_collector
 				),
+				// Nothing special to do with the cursor.
 				sge::systems::cursor_option_field()
 			)
 		)
 		(
+			// Request an image loader that can load png files.
 			sge::systems::image_loader(
 				sge::image::capabilities_field::null(),
 				fcppt::assign::make_container<
@@ -134,6 +163,13 @@ try
 		)
 	);
 
+	// Declare which sprites we want.
+	// The sprite should use int as its position and size,
+	// float as its depth, rotation, etc (which we don't use),
+	// and no color.
+	// Also request that the sprite has a dimension
+	// (meaning it is a rectangle and consists of two triangles)
+	// and that the sprite needs textures.
 	typedef sge::sprite::choices<
 		sge::sprite::type_choices<
 			int,
@@ -146,6 +182,7 @@ try
 		>
 	> sprite_choices;
 
+	// Declare the sprite system type, object type and parameters type.
 	typedef sge::sprite::system<
 		sprite_choices
 	>::type sprite_system;
@@ -162,7 +199,10 @@ try
 		sys.renderer()
 	);
 
-	sprite_object sprite1(
+	// This is our smaller sprite.
+	// It will be placed at position (200, 200) and have the size
+	// of its texture "grass.png" which is (256, 256).
+	sprite_object small_sprite(
 		sprite_parameters()
 		.pos(
 			sprite_object::vector(
@@ -171,9 +211,17 @@ try
 			)
 		)
 		.texture(
+			// Sprite uses an abstraction of textures
+			// (which is texture::part) that can refer to
+			// a part of a physical texture.
+			// We don't want that here, so we use part_raw which
+			// coveres the whole texture.
 			fcppt::make_shared_ptr<
 				sge::texture::part_raw
 			>(
+				// Create a texture from "grass.png",
+				// which will use a linear filter
+				// and clamping at its corners.
 				sge::image2d::create_texture(
 					sge::config::media_path()
 					/ FCPPT_TEXT("grass.png"),
@@ -187,11 +235,16 @@ try
 				)
 			)
 		)
+		// The sprite will get the texture's size.
 		.texture_size()
 		.elements()
 	);
 
-	sprite_object sprite2(
+	// This is our bigger sprite.
+	// It will be placed at position (0, 0) and have the size
+	// of its texture "cloudsquare.png" which is (512, 512).
+	// Everything else is pretty much the same to small_sprite.
+	sprite_object big_sprite(
 		sprite_parameters()
 		.pos(
 			sprite_object::vector::null()
@@ -217,6 +270,9 @@ try
 		.elements()
 	);
 
+	// Declare which states should be always active.
+	// Here, we clear the back buffer with the clear color black() on each frame.
+	// We also clear the stencil buffer with value 0.
 	sys.renderer().state(
 		sge::renderer::state::list
 		(
@@ -236,6 +292,7 @@ try
 
 	bool running = true;
 
+	// Set running to false when escape is pressed.
 	fcppt::signal::scoped_connection const escape_connection(
 		sys.keyboard_collector().key_callback(
 			sge::input::keyboard::action(
@@ -251,13 +308,22 @@ try
 		running
 	)
 	{
+		// Process window messages which will also process input.
 		sys.window().dispatch();
 
+		// Declare a render block.
+		// This will clear the buffers in the constructor and
+		// present the scene in the destructor.
 		sge::renderer::scoped_block const block(
 			sys.renderer()
 		);
 
 		{
+			// Set the stencil buffer to always pass and increment
+			// the value stored in the stencil buffer for every pixel rendered.
+			// Because the stencil buffer has been cleared to 0,
+			// every entry in the stencill buffer will be 1 where
+			// small_sprite is rendered.
 			sge::renderer::state::scoped const scoped_state(
 				sys.renderer(),
 				sge::renderer::state::list
@@ -270,13 +336,17 @@ try
 				)
 			);
 
+			// Render small sprite.
 			sge::sprite::render_one(
 				sprite_sys,
-				sprite1
+				small_sprite
 			);
 		}
 
 		{
+			// Set the stencil buffer to pass when its value is still 0,
+			// which means it passes everywhere where small_sprite
+			// has _not_ been rendered.
 			sge::renderer::state::scoped const scoped_state(
 				sys.renderer(),
 				sge::renderer::state::list
@@ -288,9 +358,10 @@ try
 				)
 			);
 
+			// Render big sprite.
 			sge::sprite::render_one(
 				sprite_sys,
-				sprite2
+				big_sprite
 			);
 		}
 	}	
