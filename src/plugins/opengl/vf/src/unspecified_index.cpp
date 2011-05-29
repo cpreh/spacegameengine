9d133f61adda0a370218b675bf7601606cc32b15
Philipp Reh <sefi@s-e-f-i.de>
2011-05-29 23:01:35 +0200

    Corrected glsl debug message.

--- src/plugins/opengl/vf/src/unspecified_index.cpp
+++ src/plugins/opengl/vf/src/unspecified_index.cpp
@@ -42,7 +42,7 @@ sge::opengl::vf::unspecified_index(
 			FCPPT_TEXT("Tried to set a vertex_buffer that uses unspecified elements, ")
 			FCPPT_TEXT("but a GLSL program is not set.")
 			FCPPT_TEXT(" You have to set the GLSL program before setting the vertex buffer, ")
-			FCPPT_TEXT("e.g. device->set_glsl_program(prog); device->set_vertex_buffer(buf);")
+			FCPPT_TEXT("e.g. device.glsl_program(prog); device.activate_vertex_buffer(buf);")
 		);
 	
 	return
