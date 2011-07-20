409bbe65f6e889c6b4b2c84b2756ecaebf589d62
Niels Meyering <nmeyering@gmail.com>
2011-07-20 17:01:57 +0200

    modified shader cube example

--- media/shaders/cube/fragment.glsl
+++ media/shaders/cube/fragment.glsl
@@ -13,25 +13,47 @@ main()
 		vec3(0.0,0.0,1.0);
 
 	if (enabled == 1)
-		normal_coord += 
+		normal_coord = 
+			normalize(
 			vec3(
 				texture2D(
 					normal_texture,
-					varying_texcoord));
+					varying_texcoord)));
 
-	float diffuse_term = 
+	vec3 eye_to_pos =
+		normalize(
+			camera - varying_vertex_position);
+
+	vec3 pos_to_light = 
+		normalize(
+			varying_vertex_position - varying_light_position);
+
+	float angle = 
 		clamp(
 			dot(
 				normalize(
 					normal_coord), 
-				normalize(
-					varying_light_position - varying_vertex_position)), 
-			0.0, 
+				pos_to_light
+				),
+			0.0,
 			1.0);
+
+	float diffuse_term = 
+		angle;
+
+	float spec_exponent = 50;
+	float spec_factor = 1.0;
+
+	float specular_term =
+		min(1.0,
+		spec_factor *
+			pow(max(0.0, dot(reflect(eye_to_pos, normal_coord), pos_to_light)), spec_exponent));
 
 	// Calculating The Final Color
 	gl_FragColor = 
 		texture2D(
 			color_texture,
-			varying_texcoord) * max(0.02,diffuse_term);
+			varying_texcoord)
+			* max(0.02,diffuse_term)
+		+ specular_term;
 }
