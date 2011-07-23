bc36ac5ec4fd7fab1828158005d5a1086dd50695
Philipp Reh <sefi@s-e-f-i.de>
2011-07-23 23:48:24 +0200

    Include timer/basic_fwd in timer/basic_decl to get the default template parameter.

--- include/sge/timer/basic_decl.hpp
+++ include/sge/timer/basic_decl.hpp
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef SGE_TIMER_BASIC_DECL_HPP_INCLUDED
 #define SGE_TIMER_BASIC_DECL_HPP_INCLUDED
 
+#include <sge/timer/basic_fwd.hpp>
 #include <sge/timer/time_point.hpp>
 #include <sge/timer/callback.hpp>
 #include <sge/timer/parameters_decl.hpp>
