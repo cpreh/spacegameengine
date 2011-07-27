26f48fac99da0436068c3f451935fb47ea2fb366
phillemann <pmidden@gmx.net>
2011-07-27 17:52:31 +0200

    Brought back sge::time

---
+++ include/sge/time/time.hpp
@@ -0,0 +1,44 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2011 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef SGE_TIME_TIME_HPP_INCLUDED
+#define SGE_TIME_TIME_HPP_INCLUDED
+
+#include <sge/time/activation_state.hpp>
+#include <sge/time/callback.hpp>
+#include <sge/time/clock.hpp>
+#include <sge/time/default_callback.hpp>
+#include <sge/time/duration.hpp>
+#include <sge/time/expiration_state.hpp>
+#include <sge/time/frames_counter.hpp>
+#include <sge/time/funit.hpp>
+#include <sge/time/microsecond.hpp>
+#include <sge/time/millisecond.hpp>
+#include <sge/time/nanosecond.hpp>
+#include <sge/time/now.hpp>
+#include <sge/time/point.hpp>
+#include <sge/time/second.hpp>
+#include <sge/time/second_f.hpp>
+#include <sge/time/sleep.hpp>
+#include <sge/time/symbol.hpp>
+#include <sge/time/timer.hpp>
+#include <sge/time/unit.hpp>
+
+#endif
