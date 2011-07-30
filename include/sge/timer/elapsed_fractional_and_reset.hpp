c73fae9cff519f0a0d4fa721dc812cd0830b6dad
Philipp Reh <sefi@s-e-f-i.de>
2011-07-30 16:32:08 +0200

    Added elapsed_fractional_and_reset.

--- include/sge/timer/elapsed_fractional_and_reset.hpp
+++ include/sge/timer/elapsed_fractional_and_reset.hpp
@@ -18,27 +18,42 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef SGE_TIMER_TIMER_HPP_INCLUDED
-#define SGE_TIMER_TIMER_HPP_INCLUDED
+#ifndef SGE_TIMER_ELAPSED_FRACTIONAL_AND_RESET_HPP_INCLUDED
+#define SGE_TIMER_ELAPSED_FRACTIONAL_AND_RESET_HPP_INCLUDED
 
 #include <sge/timer/basic.hpp>
-#include <sge/timer/basic_decl.hpp>
-#include <sge/timer/basic_fwd.hpp>
-#include <sge/timer/basic_impl.hpp>
-#include <sge/timer/clocks/clocks.hpp>
-#include <sge/timer/elapsed.hpp>
-#include <sge/timer/elapsed_and_reset.hpp>
 #include <sge/timer/elapsed_fractional.hpp>
-#include <sge/timer/elapsed_fractional_and_reset.hpp>
-#include <sge/timer/enable_ctor_stateful.hpp>
-#include <sge/timer/enable_ctor_stateless.hpp>
-#include <sge/timer/frames_counter.hpp>
-#include <sge/timer/parameters.hpp>
-#include <sge/timer/parameters_decl.hpp>
-#include <sge/timer/parameters_fwd.hpp>
-#include <sge/timer/parameters_impl.hpp>
-#include <sge/timer/remaining.hpp>
-#include <sge/timer/remaining_fractional.hpp>
-#include <sge/timer/reset_when_expired.hpp>
+#include <boost/type_traits/is_floating_point.hpp>
+#include <boost/utility/enable_if.hpp>
+
+namespace sge
+{
+namespace timer
+{
+template<typename Float,typename Clock>
+typename
+boost::enable_if
+<
+	boost::is_floating_point<Float>,
+	Float
+>::type
+elapsed_fractional_and_reset(
+	timer::basic<Clock> &_timer)
+{
+	Float const ret(
+		timer::elapsed_fractional<
+			Float
+		>(
+			_timer
+		)
+	);
+
+	_timer.reset();
+
+	return ret;
+
+}
+}
+}
 
 #endif
