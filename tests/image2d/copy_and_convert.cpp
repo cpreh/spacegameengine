e6593894d45c2bc726ced06c0a6bc793ebf82ae2
Philipp Reh <sefi@s-e-f-i.de>
2011-08-06 16:56:13 +0200

    Initial copy_and_convert test.

--- tests/image2d/copy_and_convert.cpp
+++ tests/image2d/copy_and_convert.cpp
@@ -18,10 +18,107 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
+#include <sge/image/color/argb8.hpp>
+#include <sge/image/color/channel8.hpp>
+#include <sge/image/color/rgba8.hpp>
+#include <sge/image/color/init.hpp>
+#include <sge/image/store.hpp>
+#include <sge/image2d/argb8.hpp>
+#include <sge/image2d/rgba8.hpp>
+#include <sge/image2d/algorithm/copy_and_convert.hpp>
+#include <sge/image2d/view/const_object.hpp>
+#include <sge/image2d/view/object.hpp>
+#include <mizuiro/image/view.hpp>
+#include <mizuiro/color/operators/equal.hpp>
+#include <fcppt/math/dim/basic_impl.hpp>
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(
-	rgb_to_argb
+namespace
+{
+
+sge::image::color::channel8
+channel8(
+	unsigned const _value
 )
 {
+	return
+		static_cast<
+			sge::image::color::channel8
+		>(
+			_value
+		);
+}
+
+}
+
+BOOST_AUTO_TEST_CASE(
+	rgba_to_argb
+)
+{
+	typedef sge::image2d::rgba8 source_store;
+
+	source_store source(
+		sge::image2d::rgba8::dim_type(
+			1,
+			1	
+		)
+	);
+
+	typedef source_store::view_type source_view_type;
+
+	source_view_type const source_view(
+		source.view()
+	);
+
+	source_view[
+		source_view_type::dim_type(
+			0,
+			0
+		)
+	] =
+		sge::image::color::rgba8(
+			(sge::image::color::init::red = channel8(0))
+			(sge::image::color::init::blue = channel8(37))
+			(sge::image::color::init::green = channel8(255))
+			(sge::image::color::init::alpha = channel8(128))
+		);
+	
+	typedef sge::image2d::argb8 dest_store;
+
+	dest_store dest(
+		source.dim()
+	);
+
+	typedef dest_store::view_type dest_view_type;
+
+	dest_view_type dest_view(
+		dest.view()
+	);
+
+	sge::image2d::algorithm::copy_and_convert(
+		sge::image2d::view::const_object(
+			source_store::const_view_type(
+				source_view
+			)
+		),
+		sge::image2d::view::object(
+			dest_view
+		)
+	);
+
+	BOOST_REQUIRE(
+		dest_view[
+			dest_view_type::dim_type(
+				0,
+				0
+			)
+		]
+		==
+		sge::image::color::argb8(
+			(sge::image::color::init::red = channel8(0))
+			(sge::image::color::init::blue = channel8(37))
+			(sge::image::color::init::green = channel8(255))
+			(sge::image::color::init::alpha = channel8(128))
+		)
+	);
 }
