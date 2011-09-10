1aee243f07f72778daa9cc0bd3e8ac78da5c3e9a
Philipp Reh <sefi@s-e-f-i.de>
2011-09-10 15:29:51 +0200

    Removed some iostream stuff.

--- src/parse/json/find_object.cpp
+++ src/parse/json/find_object.cpp
@@ -30,7 +30,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <fcppt/text.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <algorithm>
-#include <iostream>
 #include <typeinfo>
 #include <fcppt/config/external_end.hpp>
 
