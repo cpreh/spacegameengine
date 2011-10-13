63f1450f325835438af41174f134dd6ee721e9ac
Philipp Reh <sefi@s-e-f-i.de>
2011-10-13 17:22:54 +0200

    Print the error code from XIGrabDevice.

--- src/plugins/x11input/src/cursor/grab.cpp
+++ src/plugins/x11input/src/cursor/grab.cpp
@@ -22,13 +22,17 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <awl/backends/x11/display.hpp>
 #include <awl/backends/x11/window/instance.hpp>
 #include <sge/input/exception.hpp>
+#include <sge/log/global.hpp>
 #include <fcppt/assign/make_container.hpp>
-#include <fcppt/function/object.hpp>
-#include <fcppt/tr1/functional.hpp>
-#include <fcppt/text.hpp>
-#include <fcppt/time/sleep_any.hpp>
 #include <fcppt/chrono/duration_impl.hpp>
 #include <fcppt/chrono/milliseconds.hpp>
+#include <fcppt/function/object.hpp>
+#include <fcppt/log/error.hpp>
+#include <fcppt/log/output.hpp>
+#include <fcppt/time/sleep_any.hpp>
+#include <fcppt/tr1/functional.hpp>
+#include <fcppt/insert_to_fcppt_string.hpp>
+#include <fcppt/text.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <X11/extensions/XInput2.h>
 #include <fcppt/config/external_end.hpp>
@@ -53,7 +57,7 @@ sge::x11input::cursor::grab::grab(
 		;;
 	)
 	{
-		switch(
+		Status const ret(
 			::XIGrabDevice(
 				_window.display().get(),
 				_id.get(),
@@ -65,6 +69,10 @@ sge::x11input::cursor::grab::grab(
 				True,
 				&mask
 			)
+		);
+
+		switch(
+			ret
 		)
 		{
 		case Success:
@@ -72,6 +80,14 @@ sge::x11input::cursor::grab::grab(
 		case BadMatch:
 		case BadWindow:
 		case BadRequest:
+			FCPPT_LOG_ERROR(
+				sge::log::global(),
+				fcppt::log::_
+					<< FCPPT_TEXT("XIGrabDevice failed with code ")
+					<< ret
+					<< FCPPT_TEXT(". Retrying...")
+			);
+
 			fcppt::time::sleep_any(
 				fcppt::chrono::milliseconds(
 					10
@@ -80,7 +96,10 @@ sge::x11input::cursor::grab::grab(
 			break;
 		default:
 			throw sge::input::exception(
-				FCPPT_TEXT("X11 grab failed!")
+				FCPPT_TEXT("X11 grab failed with code ")
+				+ fcppt::insert_to_fcppt_string(
+					ret
+				)
 			);
 		}
 	}
