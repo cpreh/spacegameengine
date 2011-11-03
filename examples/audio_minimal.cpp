0b30dfe2badc5e0e07e2c9a444028e4b9a4dbd5d
phillemann <pmidden@gmx.net>
2011-11-03 01:02:57 +0100

    Added section about manual plugin init in audio tut

--- examples/audio_minimal.cpp
+++ examples/audio_minimal.cpp
@@ -3,10 +3,20 @@
 #include <sge/audio/loader.hpp>
 #include <sge/audio/player.hpp>
 #include <sge/audio/sound/base.hpp>
-#include <sge/systems/audio_player_default.hpp>
 #include <sge/config/media_path.hpp>
+#ifdef SGE_EXAMPLES_AUDIO_MINIMAL_USE_SYSTEMS_INIT
+#include <sge/systems/audio_player_default.hpp>
 #include <sge/systems/instance.hpp>
 #include <sge/systems/list.hpp>
+#else
+#include <sge/plugin/manager.hpp>
+#include <sge/plugin/iterator.hpp>
+#include <sge/plugin/object.hpp>
+#include <sge/plugin/context.hpp>
+#include <sge/audio/player_plugin.hpp>
+#include <sge/audio/player_ptr.hpp>
+#include <sge/config/plugin_path.hpp>
+#endif // SGE_EXAMPLES_AUDIO_MINIMAL_USE_SYSTEMS_INIT
 #include <sge/audio/loader_capabilities_field.hpp>
 #include <sge/audio/multi_loader.hpp>
 #include <fcppt/exception.hpp>
@@ -25,11 +35,12 @@
 int main(int, char *[])
 try
 {
-//! [systems_initialization]
-	// Which file types do we want to load (see below)?
 	sge::extension_set extensions;
 	extensions.insert(
 		FCPPT_TEXT("wav"));
+#ifdef SGE_EXAMPLES_AUDIO_MINIMAL_USE_SYSTEMS_INIT
+//! [systems_initialization]
+	// Which file types do we want to load (see below)?
 
 	sge::systems::instance sys(
 		sge::systems::list()
@@ -46,17 +57,54 @@ try
 			// Other system initializations (renderer, ...) here
 			// ...
 		);
-//! [systems_initialization]
 
+	sge::audio::player &player =
+		sys.audio_player();
+
+	sge::audio::multi_loader &loader =
+		sys.audio_loader();
+//! [systems_initialization]
+#else
+
+//! [manual_initialization_pm]
+	sge::plugin::manager plugin_manager(
+		sge::config::plugin_path());
+//! [manual_initialization_pm]
+//
+//! [manual_initialization_player]
+	sge::plugin::iterator<sge::audio::player> player_it =
+		plugin_manager.begin<sge::audio::player>();
+
+	typedef
+	sge::plugin::context<sge::audio::player>::ptr_type
+	player_plugin_ptr;
+
+	player_plugin_ptr const player_plugin(
+		player_it->load());
+
+	sge::audio::player_ptr const _player(
+		player_plugin->get()());
+
+	sge::audio::player &player =
+		*_player;
+//! [manual_initialization_player]
+
+//! [manual_initialization_loader]
+	sge::audio::multi_loader loader(
+		plugin_manager,
+		extensions,
+		sge::audio::loader_capabilities_field::null());
+//! [manual_initialization_loader]
+#endif
 
 	sge::audio::file_ptr const soundfile =
-		sys.audio_loader().load(
+		loader.load(
 			sge::config::media_path()
 				/ FCPPT_TEXT("sounds")
 				/ FCPPT_TEXT("ding.wav"));
 
 	sge::audio::buffer_ptr const buf =
-		sys.audio_player().create_buffer(
+		player.create_buffer(
 			*soundfile);
 
 	sge::audio::sound::base_ptr const s =
