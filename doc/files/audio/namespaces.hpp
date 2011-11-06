07ee84ab54113ab344cf4be56d9718d603cc9ae7
phillemann <pmidden@gmx.net>
2011-11-06 18:26:53 +0100

    Minor documentation changes

---
+++ doc/files/audio/namespaces.hpp
@@ -0,0 +1,16 @@
+namespace sge
+{
+/// Contains the audio subsystem, \see audio_main
+namespace audio
+{
+/// Contains the direction object used in the sge::audio::listener
+namespace direction
+{
+}
+
+/// Contains all sound related classes and functions
+namespace sound
+{
+}
+}
+}
