15ec1b57cc0b7cba097ccf58cc360ae331957e98
Philipp Reh <sefi@s-e-f-i.de>
2011-11-18 11:20:12 +0100

    Added a missing include.

--- src/camera/ortho_freelook/object.cpp
+++ src/camera/ortho_freelook/object.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/camera/projection/orthogonal.hpp>
 #include <sge/input/keyboard/key_event.hpp>
 #include <sge/input/mouse/axis_event.hpp>
+#include <fcppt/chrono/duration_impl.hpp>
 #include <fcppt/math/dim/arithmetic.hpp>
 #include <fcppt/math/vector/arithmetic.hpp>
 #include <sge/input/keyboard/device.hpp>
