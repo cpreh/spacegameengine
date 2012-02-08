24bd241aa2925d8fc3de20d2f0806a8be8090979
Philipp Reh <sefi@s-e-f-i.de>
2012-02-08 14:13:29 +0100

    Added new FindIconv.cmake from flusspferd.

--- CMakeModules/FindIconv.cmake
+++ CMakeModules/FindIconv.cmake
@@ -1,47 +1,119 @@
-INCLUDE(CheckCXXSourceRuns)
+# vim:ts=4:sw=4:expandtab:autoindent:
+#
+# The MIT License
+#
+# Copyright (c) 2008, 2009 Flusspferd contributors (see "CONTRIBUTORS" or
+#                                      http://flusspferd.org/contributors.txt)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
 
-CHECK_CXX_SOURCE_RUNS(
-        "#include <unistd.h>
-        #include <cstdlib>
-        int main()
-        {
-          return _POSIX_VERSION >= 200100
-                 ? EXIT_SUCCESS
-                 : EXIT_FAILURE;
-        }"
-        ICONV_HAVE_POSIX
-)
+include(CheckFunctionExists)
+include(CheckCXXSourceCompiles)
 
-INCLUDE(FindPackageHandleStandardArgs)
+if(ICONV_INCLUDE_DIR)
+	set(ICONV_FIND_QUIETLY TRUE)
+endif()
 
-FIND_PATH(
+find_path(ICONV_INCLUDE_DIR iconv.h)
+
+if(NOT ICONV_INCLUDE_DIR STREQUAL "ICONV_INCLUDE_DIR-NOTFOUND")
+	set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
+	check_function_exists(iconv_open ICONV_IN_LIBC)
+endif()
+
+if(NOT ICONV_IN_LIBC)
+	find_library(ICONV_LIBRARY NAMES iconv)
+	set(ICONV_TEST ${ICONV_LIBRARY})
+else()
+	set(ICONV_TEST "In glibc")
+endif()
+
+set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
+set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARY})
+
+check_cxx_source_compiles(
+	"#include <iconv.h>
+	int main() {
+		iconv(iconv_t(-1), 0, 0, 0, 0);
+	}"
+	ICONV_COMPILES)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(
+	ICONV
+	DEFAULT_MSG
+	ICONV_TEST
 	ICONV_INCLUDE_DIR
-	NAMES iconv.h
-	PATHS ${ICONV_ROOT}/include
+	ICONV_COMPILES
 )
 
-IF(
-	ICONV_HAVE_POSIX
+if(ICONV_FOUND)
+	set(ICONV_LIBRARIES ${ICONV_LIBRARY})
+else()
+	unset(ICONV_LIBRARIES)
+endif()
+
+if(ICONV_FOUND)
+	set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
+	set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
+
+	if (NOT DEFINED ICONV_ACCEPTS_NONCONST_INPUT)
+		# Display a useful message first time we come through here
+		message(STATUS "One (and only one) of the ICONV_ACCEPTS_... tests must pass")
+	endif()
+
+	check_cxx_source_compiles(
+		"#include <iconv.h>
+		int main() {
+			char *p = 0;
+			iconv(iconv_t(-1), &p, 0, 0, 0);
+		}"
+		ICONV_ACCEPTS_NONCONST_INPUT)
+
+	check_cxx_source_compiles(
+		"#include <iconv.h>
+		int main() {
+			char const *p = 0;
+			iconv(iconv_t(-1), &p, 0, 0, 0);
+		}"
+		ICONV_ACCEPTS_CONST_INPUT)
+
+	if (ICONV_LIBRARY)
+		list(REMOVE_ITEM CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARY})
+		list(REMOVE_DUPLICATES CMAKE_REQUIRED_LIBRARIES)
+	endif()
+
+	set(
+		ICONV_INCLUDE_DIRS
+		${ICONV_INCLUDE_DIR}
+	)
+endif()
+
+if(NOT ICONV_ACCEPTS_CONST_INPUT AND NOT ICONV_ACCEPTS_NONCONST_INPUT)
+	MESSAGE(FATAL_ERROR "Unable to determine iconv() signature")
+elseif(ICONV_ACCEPTS_CONST_INPUT AND ICONV_ACCEPTS_NONCONST_INPUT)
+	MESSAGE(FATAL_ERROR "Unable to determine iconv() signature - both test cases passed!")
+endif()
+
+mark_as_advanced(
+	ICONV_LIBRARY
+	ICONV_INCLUDE_DIR
 )
-	FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-		ICONV
-		DEFAULT_MSG
-		ICONV_INCLUDE_DIR
-	)
-ELSE()
-
-	FIND_LIBRARY(
-		ICONV_LIBRARY
-		NAMES libiconv
-		PATHS ${ICONV_ROOT}/lib
-	)
-
-	FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-		ICONV
-		DEFAULT_MSG
-		ICONV_INCLUDE_DIR
-		ICONV_LIBRARY
-	)
-ENDIF()
-
-# TODO: check for the iconv signature
