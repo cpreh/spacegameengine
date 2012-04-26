41072a9b415d613fa035f2006686e5c522a81cef
Niels Meyering <nmeyering@gmail.com>
2012-04-26 22:48:41 +0200

    noise example now renders and displays a texture

--- examples/noise.cpp
+++ examples/noise.cpp
@@ -17,21 +17,130 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-
-#include <sge/noise/simplex/object.hpp>
+#include <sge/image/colors.hpp>
+#include <sge/image/store.hpp>
+#include <sge/image/color/channel8.hpp>
+#include <sge/image2d/dim.hpp>
+#include <sge/image2d/l8.hpp>
+#include <sge/image2d/view/const_object.hpp>
+#include <sge/image2d/view/object.hpp>
+#include <sge/log/global.hpp>
 #include <sge/noise/sample.hpp>
 #include <sge/noise/sample_parameters.hpp>
+#include <sge/noise/simplex/object.hpp>
+#include <sge/renderer/bit_depth.hpp>
+#include <sge/renderer/depth_stencil_buffer.hpp>
+#include <sge/renderer/device.hpp>
+#include <sge/renderer/no_multi_sampling.hpp>
+#include <sge/renderer/parameters.hpp>
+#include <sge/renderer/resource_flags.hpp>
+#include <sge/renderer/resource_flags_none.hpp>
+#include <sge/renderer/scoped_block.hpp>
+#include <sge/renderer/vsync.hpp>
+#include <sge/renderer/windowed.hpp>
+#include <sge/renderer/texture/create_planar_from_view.hpp>
+#include <sge/renderer/texture/planar_scoped_ptr.hpp>
+#include <sge/renderer/texture/mipmap/off.hpp>
+#include <sge/sprite/default_parameters.hpp>
+#include <sge/sprite/object.hpp>
+#include <sge/sprite/parameters.hpp>
+#include <sge/sprite/buffers/option.hpp>
+#include <sge/sprite/buffers/single.hpp>
+#include <sge/sprite/buffers/with_declaration.hpp>
+#include <sge/sprite/config/choices.hpp>
+#include <sge/sprite/config/float_type.hpp>
+#include <sge/sprite/config/normal_size.hpp>
+#include <sge/sprite/config/texture_coordinates.hpp>
+#include <sge/sprite/config/texture_level_count.hpp>
+#include <sge/sprite/config/texture_ownership.hpp>
+#include <sge/sprite/config/type_choices.hpp>
+#include <sge/sprite/config/unit_type.hpp>
+#include <sge/sprite/config/with_texture.hpp>
+#include <sge/sprite/defaults/defaults.hpp>
+#include <sge/sprite/process/one.hpp>
+#include <sge/systems/instance.hpp>
+#include <sge/systems/list.hpp>
+#include <sge/systems/renderer.hpp>
+#include <sge/systems/window.hpp>
+#include <sge/texture/const_part_shared_ptr.hpp>
+#include <sge/texture/part_raw.hpp>
+#include <sge/viewport/fill_on_resize.hpp>
+#include <sge/window/dim.hpp>
+#include <sge/window/parameters.hpp>
+#include <sge/window/system.hpp>
+#include <sge/window/title.hpp>
 #include <awl/main/exit_code.hpp>
 #include <awl/main/exit_failure.hpp>
-#include <awl/main/exit_success.hpp>
 #include <awl/main/function_context.hpp>
+#include <mizuiro/color/channel/luminance.hpp>
 #include <fcppt/exception.hpp>
+#include <fcppt/make_shared_ptr.hpp>
+#include <fcppt/ref.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/io/cerr.hpp>
 #include <fcppt/io/cout.hpp>
+#include <fcppt/log/activate_levels.hpp>
+#include <fcppt/log/level.hpp>
+#include <fcppt/math/dim/structure_cast.hpp>
 #include <fcppt/config/external_begin.hpp>
+#include <exception>
 #include <iostream>
 #include <fcppt/config/external_end.hpp>
+
+namespace
+{
+
+void
+fill_texture(
+	sge::image2d::l8::view_type _view)
+{
+	typedef sge::image2d::l8::view_type::dim dim_type;
+	typedef dim_type::value_type dim_value_type;
+
+	typedef
+	sge::noise::simplex::object<double,2>
+	noise_type;
+
+	typedef
+	sge::noise::sample_parameters<noise_type>
+	param_type;
+
+	noise_type noise_generator(128);
+
+	dim_type const dim = _view.size();
+
+	for (dim_value_type y = 0; y < _view.size()[1]; ++y)
+		for (dim_value_type x = 0; x < _view.size()[0]; ++x)
+		{
+			_view[
+				sge::image2d::l8::view_type::dim(
+					x,
+					y
+				)
+			].set(
+				mizuiro::color::channel::luminance(),
+				static_cast<sge::image::color::channel8>(
+					256.0 *
+					(0.5 + 0.5 *
+					sge::noise::sample(
+						noise_generator,
+						param_type(
+							param_type::position_type(
+								noise_type::vector_type(
+									x,
+									y)),
+							param_type::amplitude_type(
+								0.8),
+							param_type::frequency_type(
+								.005),
+							param_type::octaves_type(
+								6u))))
+				)
+			);
+		}
+}
+
+}
 
 awl::main::exit_code const
 example_main(
@@ -52,47 +161,139 @@ try
 					42.0))
 		<< FCPPT_TEXT('\n');
 
-	typedef
-	sge::noise::sample_parameters<noise_type>
-	param_type;
+	fcppt::log::activate_levels(
+		sge::log::global(),
+		fcppt::log::level::debug
+	);
 
-	fcppt::io::cout()
-		<< FCPPT_TEXT("noise generator spit out: ")
-		<<
-			sge::noise::sample(
-				noise_generator,
-				param_type(
-					param_type::position_type(
-						noise_type::vector_type(
-							23.0,
-							42.0)),
-					param_type::amplitude_type(
-						1.0),
-					param_type::frequency_type(
-						10.0),
-					param_type::octaves_type(
-						5u)))
-		<< FCPPT_TEXT('\n');
+	sge::window::dim const window_dim(
+		1024,
+		1024
+	);
 
-	return awl::main::exit_success();
+	sge::systems::instance const sys(
+		sge::systems::list()
+		(sge::systems::window(
+				sge::window::parameters(
+					sge::window::title(
+						FCPPT_TEXT("noisetest")
+					),
+					window_dim)))
+		(sge::systems::renderer(
+				sge::renderer::parameters(
+					sge::renderer::windowed(
+						sge::renderer::bit_depth::depth32),
+					sge::renderer::depth_stencil_buffer::off,
+					sge::renderer::vsync::on,
+					sge::renderer::no_multi_sampling),
+				sge::viewport::fill_on_resize()))
+	);
+
+	typedef sge::sprite::config::choices<
+		sge::sprite::config::type_choices<
+			sge::sprite::config::unit_type<
+				int
+			>,
+			sge::sprite::config::float_type<
+				float
+			>
+		>,
+		sge::sprite::config::normal_size,
+		boost::mpl::vector1<
+			sge::sprite::config::with_texture<
+				sge::sprite::config::texture_level_count<
+					1u
+				>,
+				sge::sprite::config::texture_coordinates::automatic,
+				sge::sprite::config::texture_ownership::shared
+			>
+		>
+	> sprite_choices;
+
+	typedef sge::sprite::buffers::with_declaration<
+		sge::sprite::buffers::single<
+			sprite_choices
+		>
+	> sprite_buffers_type;
+
+	typedef sge::sprite::object<
+		sprite_choices
+	> sprite_object;
+
+	typedef sge::sprite::parameters<
+		sprite_choices
+	> sprite_parameters;
+
+	sprite_buffers_type sprite_buffers(
+		sys.renderer(),
+		sge::sprite::buffers::option::dynamic);
+
+	sge::image2d::l8 store(
+		fcppt::math::dim::structure_cast<
+			sge::image2d::dim
+		>(
+			window_dim));
+
+	fill_texture(
+		store.view());
+
+	sge::renderer::texture::planar_scoped_ptr const noise_texture(
+		sge::renderer::texture::create_planar_from_view(
+			sys.renderer(),
+			sge::image2d::view::const_object(
+				sge::image2d::view::object(
+					store.wrapped_view())),
+			sge::renderer::texture::mipmap::off(),
+			sge::renderer::resource_flags::none));
+
+	sprite_object const spr(
+		sge::sprite::default_parameters<
+			sprite_choices
+		>()
+		.size(
+			sprite_object::dim(
+				fcppt::math::dim::structure_cast<
+					sprite_object::dim
+				>(
+					window_dim
+				)
+			)
+		)
+		.texture(
+			fcppt::make_shared_ptr<
+				sge::texture::part_raw
+			>(
+				fcppt::ref(
+					*noise_texture
+				)
+			)
+		)
+	);
+
+	while(
+		sys.window_system().poll()
+	)
+	{
+		sge::sprite::process::one(
+			spr,
+			sprite_buffers
+		);
+
+		sge::renderer::scoped_block const block_(
+			sys.renderer());
+	}
+
+	return sys.window_system().exit_code();
 }
-catch(
-	fcppt::exception const &_error
-)
+catch(fcppt::exception const &e)
 {
-	fcppt::io::cerr()
-		<< _error.string()
-		<< FCPPT_TEXT('\n');
-
+	fcppt::io::cerr() << e.string() << FCPPT_TEXT("\n");
 	return awl::main::exit_failure();
 }
 catch(
 	std::exception const &_error
 )
 {
-	std::cerr
-		<< _error.what()
-		<< '\n';
-
+	std::cerr << _error.what() << '\n';
 	return awl::main::exit_failure();
 }
