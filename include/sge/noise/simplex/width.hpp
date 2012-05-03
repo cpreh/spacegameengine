b59024c28559c12948d2dc5178d799f301ba7d37
phillemann <pmidden@gmx.net>
2012-05-03 12:43:46 +0200

    Added missing width.hpp file for noise

---
+++ include/sge/noise/simplex/width.hpp
@@ -0,0 +1,20 @@
+#ifndef SGE_NOISE_SIMPLEX_WIDTH_HPP_INCLUDED
+#define SGE_NOISE_SIMPLEX_WIDTH_HPP_INCLUDED
+
+#include <cstddef>
+#include <fcppt/strong_typedef.hpp>
+
+namespace sge
+{
+namespace noise
+{
+namespace simplex
+{
+FCPPT_MAKE_STRONG_TYPEDEF(
+	std::size_t,
+	width);
+}
+}
+}
+
+#endif
