1920cc6f82e5921839601a586273d08ca0d64755
Philipp Reh <sefi@s-e-f-i.de>
2012-06-07 19:07:32 +0200

    Added scoped_frame_limiter

---
+++ include/sge/timer/scoped_frame_limiter.hpp
@@ -0,0 +1,81 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2012 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef SGE_TIMER_SCOPED_FRAME_LIMITER_HPP_INCLUDED
+#define SGE_TIMER_SCOPED_FRAME_LIMITER_HPP_INCLUDED
+
+#include <fcppt/noncopyable.hpp>
+#include <sge/timer/scoped_frame_limiter_fwd.hpp>
+#include <sge/timer/symbol.hpp>
+#include <sge/timer/clocks/standard.hpp>
+
+
+namespace sge
+{
+namespace timer
+{
+/**
+\brief Force a code block to execute at least a given number of milliseconds
+
+This class is used to artificially limit the frame rate or the duration of one
+simulation "tick" to a specified amount of milliseconds.
+*/
+class scoped_frame_limiter
+{
+	FCPPT_NONCOPYABLE(
+		scoped_frame_limiter
+	);
+public:
+	typedef
+	sge::timer::clocks::standard
+	clock_type;
+
+	typedef
+	clock_type::rep
+	ticks_per_second;
+
+	/**
+	\brief Notes the current time for later comparison
+	\param f Denotes the targeted number of ticks per seconds
+
+	Any frame that's shorter than 1/f will be stalled (by calling sleep, so
+	no CPU is consumed).
+	*/
+	SGE_TIMER_SYMBOL
+	explicit
+	scoped_frame_limiter(
+		ticks_per_second
+	);
+
+	/**
+	\brief Compares the stored time with the current time and calls sleep
+	*/
+	SGE_TIMER_SYMBOL
+	~scoped_frame_limiter();
+private:
+	clock_type::time_point const before_frame_;
+
+	clock_type::duration minimum_frame_length_;
+};
+
+}
+}
+
+#endif
---
+++ src/timer/scoped_frame_limiter.cpp
@@ -0,0 +1,61 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2012 Carl Philipp Reh (sefi@s-e-f-i.de)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include <sge/timer/scoped_frame_limiter.hpp>
+#include <fcppt/time/sleep_any.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/chrono/duration.hpp>
+#include <fcppt/config/external_end.hpp>
+
+
+sge::timer::scoped_frame_limiter::scoped_frame_limiter(
+	ticks_per_second const _desired_fps
+)
+:
+	before_frame_(
+		clock_type::now()
+	),
+	minimum_frame_length_(
+		boost::chrono::duration_cast<
+			clock_type::duration
+		>(
+			boost::chrono::seconds(1)
+		)
+		/
+		_desired_fps
+	)
+{
+}
+
+sge::timer::scoped_frame_limiter::~scoped_frame_limiter()
+{
+	clock_type::duration const diff(
+		clock_type::now() - before_frame_
+	);
+
+	if(
+		diff > minimum_frame_length_
+	)
+		return;
+
+	fcppt::time::sleep_any(
+		minimum_frame_length_ - diff
+	);
+}
