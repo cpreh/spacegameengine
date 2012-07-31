98fd33aa91391880d3e3704510cb1427e2fc7539
Philipp Middendorf <pmidden@gmx.net>
2012-07-31 17:57:01 +0200

    Use a helper structure to improve obj loading performance

--- include/sge/model/obj/face_vertex.hpp
+++ include/sge/model/obj/face_vertex.hpp
@@ -25,6 +25,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/model/obj/symbol.hpp>
 #include <sge/model/obj/texture_coordinate_index.hpp>
 #include <sge/model/obj/vertex_coordinate_index.hpp>
+#include <fcppt/container/array.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/functional/hash.hpp>
+#include <cstddef>
+#include <fcppt/config/external_end.hpp>
 
 
 namespace sge
@@ -43,31 +48,55 @@ public:
 		sge::model::obj::normal_index const &);
 
 	SGE_MODEL_OBJ_SYMBOL
-	sge::model::obj::vertex_coordinate_index const &
+	sge::model::obj::vertex_coordinate_index const
 	vertex_coordinate_index() const;
 
 	SGE_MODEL_OBJ_SYMBOL
-	sge::model::obj::texture_coordinate_index const &
+	sge::model::obj::texture_coordinate_index const
 	texture_coordinate_index() const;
 
 	SGE_MODEL_OBJ_SYMBOL
-	sge::model::obj::normal_index const &
+	sge::model::obj::normal_index const
 	normal_index() const;
 
-	/*
-	bool
-	operator<(
-		face_vertex const &) const;
-	*/
-
-	SGE_MODEL_OBJ_SYMBOL
 	bool
 	operator==(
-		face_vertex const &) const;
+		face_vertex const &_other) const
+	{
+		return
+			indices_ == _other.indices_;
+	}
+
+	bool
+	operator<(
+		face_vertex const &_other) const
+	{
+		return
+			indices_ < _other.indices_;
+	}
+
+	friend std::size_t
+	hash_value(
+		face_vertex const &_vertex)
+	{
+		std::size_t seed = 0u;
+		boost::hash_combine(
+			seed,
+			_vertex.indices_[0u]);
+		boost::hash_combine(
+			seed,
+			_vertex.indices_[1u]);
+		boost::hash_combine(
+			seed,
+			_vertex.indices_[2u]);
+		return seed;
+	}
 private:
-	sge::model::obj::vertex_coordinate_index vertex_coordinate_index_;
-	sge::model::obj::texture_coordinate_index texture_coordinate_index_;
-	sge::model::obj::normal_index normal_index_;
+	typedef
+	fcppt::container::array<std::size_t,3>
+	index_array;
+
+	index_array indices_;
 };
 }
 }
--- src/model/obj/face_vertex.cpp
+++ src/model/obj/face_vertex.cpp
@@ -6,36 +6,41 @@ sge::model::obj::face_vertex::face_vertex(
 	sge::model::obj::texture_coordinate_index const &_texture_coordinate_index,
 	sge::model::obj::normal_index const &_normal_index)
 :
-	vertex_coordinate_index_(
-		_vertex_coordinate_index),
-	texture_coordinate_index_(
-		_texture_coordinate_index),
-	normal_index_(
-		_normal_index)
+	indices_()
 {
+	indices_[0] =
+		_vertex_coordinate_index.get();
+	indices_[1] =
+		_texture_coordinate_index.get();
+	indices_[2] =
+		_normal_index.get();
 }
 
-sge::model::obj::vertex_coordinate_index const &
+sge::model::obj::vertex_coordinate_index const
 sge::model::obj::face_vertex::vertex_coordinate_index() const
 {
 	return
-		vertex_coordinate_index_;
+		sge::model::obj::vertex_coordinate_index(
+			indices_[0u]);
 }
 
-sge::model::obj::texture_coordinate_index const &
+sge::model::obj::texture_coordinate_index const
 sge::model::obj::face_vertex::texture_coordinate_index() const
 {
 	return
-		texture_coordinate_index_;
+		sge::model::obj::texture_coordinate_index(
+			indices_[1u]);
 }
 
-sge::model::obj::normal_index const &
+sge::model::obj::normal_index const
 sge::model::obj::face_vertex::normal_index() const
 {
 	return
-		normal_index_;
+		sge::model::obj::normal_index(
+			indices_[2u]);
 }
 
+/*
 bool
 sge::model::obj::face_vertex::operator==(
 	face_vertex const &_other) const
@@ -45,3 +50,4 @@ sge::model::obj::face_vertex::operator==(
 		texture_coordinate_index_ == _other.texture_coordinate_index_ &&
 		normal_index_ == _other.normal_index_;
 }
+*/
