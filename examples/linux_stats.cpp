9bcfd030050a291d91964e4e47d9a44e6b4c1ca9
Philipp Middendorf <pmidden@gmx.net>
2012-08-06 16:51:06 +0200

    Added Linux CPU usage graph example

--- examples/linux_stats.cpp
+++ examples/linux_stats.cpp
@@ -31,6 +31,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/renderer/parameters/vsync.hpp>
 #include <sge/renderer/pixel_format/color.hpp>
 #include <sge/renderer/pixel_format/depth_stencil.hpp>
+#include <awl/main/function_context.hpp>
+#include <sge/timer/scoped_frame_limiter.hpp>
 #include <sge/renderer/pixel_format/object.hpp>
 #include <sge/renderer/pixel_format/optional_multi_samples.hpp>
 #include <sge/renderer/pixel_format/srgb.hpp>
@@ -46,37 +48,184 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/window/title.hpp>
 #include <awl/main/exit_code.hpp>
 #include <awl/main/exit_failure.hpp>
-#include <awl/main/function_context.hpp>
 #include <fcppt/exception.hpp>
 #include <fcppt/io/cerr.hpp>
+#include <fcppt/extract_from_string_exn.hpp>
+#include <fcppt/math/dim/structure_cast.hpp>
+#include <fcppt/assert/error.hpp>
 #include <fcppt/log/activate_levels.hpp>
 #include <fcppt/log/level.hpp>
-#include <fcppt/random/variate.hpp>
-#include <fcppt/random/distribution/uniform_real.hpp>
-#include <fcppt/random/distribution/uniform_real.hpp>
-#include <fcppt/random/generator/minstd_rand.hpp>
-#include <fcppt/random/generator/seed_from_chrono.hpp>
+#include <fcppt/container/array.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <example_main.hpp>
 #include <exception>
 #include <iostream>
+#include <boost/utility/enable_if.hpp>
+#include <boost/type_traits/is_floating_point.hpp>
 #include <fstream>
+#include <numeric>
 #include <fcppt/config/external_end.hpp>
+#include <fcppt/assert/pre.hpp>
 
+
+namespace
+{
+class jiffies
+{
+public:
+	typedef
+	unsigned long
+	value_type;
+
+	FCPPT_MAKE_STRONG_TYPEDEF(
+		value_type,
+		total_type);
+
+	FCPPT_MAKE_STRONG_TYPEDEF(
+		value_type,
+		work_type);
+
+	jiffies(
+		total_type const &_total,
+		work_type const &_work)
+	:
+		total_(
+			_total),
+		work_(
+			_work)
+	{
+	}
+
+	total_type const
+	total() const
+	{
+		return
+			total_;
+	}
+
+	work_type const
+	work() const
+	{
+		return
+			work_;
+	}
+
+	jiffies const
+	operator-(
+		jiffies const &_other) const
+	{
+		return
+			jiffies(
+				this->total() - _other.total(),
+				this->work() - _other.work());
+	}
+
+	template<typename Float>
+	typename
+	boost::enable_if
+	<
+		boost::is_floating_point<Float>,
+		Float
+	>::type
+	work_percentage() const
+	{
+		if(!total_.get())
+			return
+				static_cast<Float>(
+					0.0f);
+
+		return
+			static_cast<Float>(
+				work_.get()) /
+			static_cast<Float>(
+				total_.get()) *
+			static_cast<Float>(
+				100.0f);
+	}
+private:
+	total_type total_;
+	work_type work_;
+};
+
+// Source: http://stackoverflow.com/questions/3017162/how-to-get-total-cpu-usage-in-linux-c
+jiffies const
+count_jiffies()
+{
+	std::ifstream cpuinfo(
+		"/proc/stat");
+
+	FCPPT_ASSERT_ERROR(
+		cpuinfo.is_open());
+
+	std::string first_word;
+	cpuinfo >> first_word;
+
+	FCPPT_ASSERT_ERROR(
+		first_word == "cpu");
+
+	typedef
+	fcppt::container::array<jiffies::value_type,7u>
+	jiffies_array;
+
+	jiffies_array current_jiffies;
+
+	for(
+		jiffies_array::iterator it =
+			current_jiffies.begin();
+		it != current_jiffies.end();
+		++it)
+		if(!(cpuinfo >> (*it)))
+			std::terminate();
+
+	return
+		jiffies(
+			jiffies::total_type(
+				std::accumulate(
+					current_jiffies.begin(),
+					current_jiffies.end(),
+					0u)),
+			jiffies::work_type(
+				std::accumulate(
+					current_jiffies.begin(),
+					current_jiffies.begin() + 3,
+					0u)));
+}
+}
 
 awl::main::exit_code const
 example_main(
-	awl::main::function_context const &)
+	awl::main::function_context const &_context)
 try
 {
+	if(_context.argc() <= 3)
+	{
+		std::cerr
+			<< "Usage: "
+			<<
+				_context.argv()[0]
+			<<
+				" <frames-per-second> <width> <height>\n";
+
+		return
+			awl::main::exit_failure();
+	}
+
+	sge::timer::scoped_frame_limiter::ticks_per_second const desired_fps(
+		fcppt::extract_from_string_exn<sge::timer::scoped_frame_limiter::ticks_per_second>(
+			std::string(
+				_context.argv()[1])));
+
+	sge::window::dim const window_dim(
+		fcppt::extract_from_string_exn<sge::window::dim::value_type>(
+			std::string(
+				_context.argv()[2])),
+		fcppt::extract_from_string_exn<sge::window::dim::value_type>(
+			std::string(
+				_context.argv()[3])));
+
 	fcppt::log::activate_levels(
 		sge::log::global(),
 		fcppt::log::level::debug
-	);
-
-	sge::window::dim const window_dim(
-		1024,
-		768
 	);
 
 	sge::systems::instance const sys(
@@ -104,64 +253,49 @@ try
 	sge::graph::object graph(
 		sge::graph::position(
 			sge::renderer::vector2(
-				100,
-				100)),
-		sge::image2d::dim(
-			512u,
-			128u
-		),
+				0.0f,
+				0.0f)),
+		fcppt::math::dim::structure_cast<sge::image2d::dim>(
+			window_dim),
 		sys.renderer(),
 		sge::graph::foreground_color(
 			sge::image::colors::white()),
 		sge::graph::background_color(
 			sge::image::colors::darkslategray()),
 		sge::graph::baseline(
-			20.0),
-		sge::graph::optional_axis_constraint());
+			50.0),
+		sge::graph::optional_axis_constraint(
+			sge::graph::axis_constraint(
+				0.0,
+				100.0)));
 
-	typedef fcppt::random::generator::minstd_rand generator_type;
+	jiffies last_jiffies =
+		count_jiffies();
 
-	generator_type generator(
-		fcppt::random::generator::seed_from_chrono<
-			generator_type::seed
-		>()
-	);
-
-	typedef fcppt::random::distribution::uniform_real<
-		double
-	> uniform_real;
-
-	typedef fcppt::random::variate<
-		generator_type,
-		uniform_real
-	> variate;
-
-	variate rng(
-		generator,
-		uniform_real(
-			uniform_real::min(
-				-100.
-			),
-			uniform_real::sup(
-				100.
-			)
-		)
-	);
 
 	while(
 		sys.window_system().poll()
 	)
 	{
+		sge::timer::scoped_frame_limiter limiter(
+			desired_fps);
+
 		sge::renderer::context::scoped const scoped_block(
 			sys.renderer(),
 			sys.renderer().onscreen_target()
 		);
 
-		graph.push(rng());
+		jiffies const current_jiffies =
+			count_jiffies();
+		graph.push(
+			(current_jiffies - last_jiffies).work_percentage<sge::graph::scalar>());
+		last_jiffies =
+			current_jiffies;
 
 		graph.render(
 			scoped_block.get()
 		);
+
 	}
 
 	return sys.window_system().exit_code();
