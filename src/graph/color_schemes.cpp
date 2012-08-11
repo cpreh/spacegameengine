7ca46696f91fd77714ff9aa36f0c7229a5afc6ae
Niels Meyering <nmeyering@gmail.com>
2012-08-11 15:37:22 +0200

    fixed color schemes

--- src/graph/color_schemes.cpp
+++ src/graph/color_schemes.cpp
@@ -7,20 +7,20 @@
 #include <sge/image/colors.hpp>
 #include <sge/graph/baseline_color.hpp>
 
-sge::graph::color_scheme const &
+sge::graph::color_scheme const
 sge::graph::color_schemes::default_()
 {
 	return
 	sge::graph::color_scheme(
 		sge::graph::foreground_color(
-			sge::image::colors::black()),
+			sge::image::colors::gray()),
 		sge::graph::foreground_alt_color(
-			sge::image::colors::darkblue()),
+			sge::image::colors::gray()),
 		sge::graph::background_color(
-			sge::image::colors::white()),
+			sge::image::colors::transparent()),
 		sge::graph::background_alt_color(
-			sge::image::colors::lightgray()),
+			sge::image::colors::black()),
 		sge::graph::baseline_color(
-			sge::image::colors::yellow())
+			sge::image::colors::lightgreen())
 	);
 }
