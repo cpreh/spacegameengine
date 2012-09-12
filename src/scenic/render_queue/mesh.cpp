763e945f7646330fdd2d6fc0e327d3c02ce5b8fd
Philipp Middendorf <pmidden@gmx.net>
2012-09-12 12:34:52 +0200

    Use the fcppt.number_multiplexer

--- src/scenic/render_queue/mesh.cpp
+++ src/scenic/render_queue/mesh.cpp
@@ -19,24 +19,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 #include <sge/scenic/render_queue/mesh.hpp>
+#include <fcppt/number_multiplexer/object.hpp>
 
 namespace
 {
-template<typename T>
-T
-set_bits(
-	T const begin,
-	T const size,
-	T const value,
-	T const input)
-{
-	T const part =
-		((1u << begin) - 1u) ^ ((1u << (begin + size)) - 1u);
-
-	return
-		(input & ~part) | (value << begin);
-}
-
 sge::scenic::render_queue::sort_index
 create_sort_index(
 	sge::scenic::render_queue::index_type const _material,
@@ -44,28 +30,33 @@ create_sort_index(
 	sge::scenic::render_queue::index_type const _diffuse_texture,
 	sge::scenic::render_queue::index_type const _specular_texture)
 {
+	typedef
+	fcppt::number_multiplexer::object<sge::scenic::render_queue::sort_index>
+	multiplexer;
+
 	return
-		set_bits<sge::scenic::render_queue::sort_index>(
-			3u,
-			20u,
-			static_cast<sge::scenic::render_queue::sort_index>(
-				_material),
-			set_bits<sge::scenic::render_queue::sort_index>(
-				43u,
-				20u,
+		multiplexer()
+			.append(
+				multiplexer::bit_count(
+					20u),
 				static_cast<sge::scenic::render_queue::sort_index>(
-					_vertex_buffer),
-				set_bits<sge::scenic::render_queue::sort_index>(
-					23u,
-					10u,
-					static_cast<sge::scenic::render_queue::sort_index>(
-						_diffuse_texture),
-					set_bits<sge::scenic::render_queue::sort_index>(
-						33u,
-						10u,
-						static_cast<sge::scenic::render_queue::sort_index>(
-							_specular_texture),
-						0u))));
+					_material))
+			.append(
+				multiplexer::bit_count(
+					20u),
+				static_cast<sge::scenic::render_queue::sort_index>(
+					_vertex_buffer))
+			.append(
+				multiplexer::bit_count(
+					10u),
+				static_cast<sge::scenic::render_queue::sort_index>(
+					_diffuse_texture))
+			.append(
+				multiplexer::bit_count(
+					10u),
+				static_cast<sge::scenic::render_queue::sort_index>(
+					_specular_texture))
+			.value();
 }
 }
 
