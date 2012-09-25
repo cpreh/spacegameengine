bfb0a0a950ee6d9874e49861a03a5c70ec282024
Philipp Reh <sefi@s-e-f-i.de>
2012-09-25 21:13:32 +0200

    Fix warning in opengl when error checking is disabled

--- src/plugins/opengl/include/sge/opengl/state/check_error.hpp
+++ src/plugins/opengl/include/sge/opengl/state/check_error.hpp
@@ -21,10 +21,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifndef SGE_OPENGL_STATE_CHECK_ERROR_HPP_INCLUDED
 #define SGE_OPENGL_STATE_CHECK_ERROR_HPP_INCLUDED
 
+#include <sge/opengl/check_state_always.hpp>
+#include <fcppt/string.hpp>
+#if defined(SGE_OPENGL_CHECK_STATE_ALWAYS)
 #include <sge/opengl/check_state.hpp>
 #include <sge/renderer/exception.hpp>
-#include <fcppt/string.hpp>
 #include <fcppt/text.hpp>
+#endif
 
 
 namespace sge
@@ -37,13 +40,19 @@ namespace state
 inline
 void
 check_error(
+#if defined(SGE_OPENGL_CHECK_STATE_ALWAYS)
 	fcppt::string const &_what
+#else
+	fcppt::string const &
+#endif
 )
 {
+#if defined(SGE_OPENGL_CHECK_STATE_ALWAYS)
 	SGE_OPENGL_CHECK_STATE(
 		_what + FCPPT_TEXT(" failed!"),
 		sge::renderer::exception
 	)
+#endif
 }
 
 }
