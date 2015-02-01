62f80dd102ae42ac33c41ed71e6a3a1e4e8f3133
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-02-01 10:41:54 +0100

    Embed bresenham in graph.cpp

--- src/graph/object.cpp
+++ src/graph/object.cpp
@@ -52,7 +52,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/sprite/state/parameters_impl.hpp>
 #include <sge/src/graph/detail/draw_visitor.hpp>
 #include <sge/texture/part_raw_ref.hpp>
-#include <mizuiro/image/algorithm/bresenham.hpp>
+#include <mizuiro/color/operators.hpp>
 #include <fcppt/make_shared_ptr.hpp>
 #include <fcppt/nonassignable.hpp>
 #include <fcppt/cast/float_to_int_fun.hpp>
@@ -62,12 +62,90 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <fcppt/config/external_begin.hpp>
 #include <boost/algorithm/minmax_element.hpp>
 #include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <type_traits>
 #include <utility>
 #include <fcppt/config/external_end.hpp>
 
 
 namespace
 {
+
+template<
+	typename View,
+	typename Color
+>
+void
+bresenham
+(
+	View const &v,
+	typename View::dim const &start,
+	typename View::dim const &end,
+	Color const &c0,
+	Color const &c1
+)
+{
+	typedef typename
+	std::make_signed
+	<
+		typename View::dim::value_type
+	>::type
+	int_type;
+
+	int_type x0 = static_cast<int_type>(start[0]);
+	int_type y0 = static_cast<int_type>(start[1]);
+	int_type x1 = static_cast<int_type>(end[0]);
+	int_type y1 = static_cast<int_type>(end[1]);
+
+	bool const steep = std::abs(y1 - y0) > std::abs(x1 - x0);
+	if (steep){
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+	}
+	if (x0 > x1)
+	{
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+
+	int_type const deltax = x1 - x0;
+	int_type const deltay = std::abs(y1 - y0);
+	int_type error = deltax / 2;
+	int_type y = y0;
+	int_type const ystep = (y0 < y1) ? 1 : -1;
+
+	double length =
+			static_cast<double>(x1 - x0) *
+			static_cast<double>(x1 - x0) +
+			static_cast<double>(y1 - y0) *
+			static_cast<double>(y1 - y0);
+
+	for (int_type x = x0; x <= x1; ++x)
+	{
+		double pos =
+				static_cast<double>(x - x0) *
+				static_cast<double>(x - x0) +
+				static_cast<double>(y - y0) *
+				static_cast<double>(y - y0);
+		double t = std::sqrt(pos / length);
+
+		Color c = t * c1 + (1.0 - t) * c0;
+
+		if (steep)
+			v[typename View::dim(static_cast<typename View::dim::value_type>(y), static_cast<typename View::dim::value_type>(x))] = c;
+		else
+			v[typename View::dim(static_cast<typename View::dim::value_type>(x), static_cast<typename View::dim::value_type>(y))] = c;
+		error -= deltay;
+		if (error < 0)
+		{
+			y += ystep;
+			error += deltax;
+		}
+	}
+}
+
+
 template<typename View,typename Color>
 void
 adapted_bresenham(
@@ -77,7 +155,7 @@ adapted_bresenham(
 	Color const &_color1,
 	Color const &_color2)
 {
-	mizuiro::image::algorithm::bresenham(
+	::bresenham(
 		_view,
 		typename View::dim(
 			_pos1.x(),
