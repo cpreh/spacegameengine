b4d3240c1385426e2b8ce0e17fe34ece04902c09
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-07-24 16:26:54 +0200

    Added function to opengl that doesn't accept funptrs

--- src/plugins/opengl/include/sge/opengl/call.hpp
+++ src/plugins/opengl/include/sge/opengl/call.hpp
@@ -18,32 +18,33 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#include <sge/opengl/call.hpp>
-#include <sge/opengl/check_state.hpp>
-#include <sge/opengl/common.hpp>
-#include <sge/opengl/enable.hpp>
-#include <sge/renderer/exception.hpp>
-#include <fcppt/format.hpp>
-#include <fcppt/text.hpp>
+#ifndef SGE_OPENGL_CALL_HPP_INCLUDED
+#define SGE_OPENGL_CALL_HPP_INCLUDED
 
 
-void
-sge::opengl::enable(
-	GLenum const _what
+namespace sge
+{
+namespace opengl
+{
+
+template<
+	typename Result,
+	typename... Args
+>
+inline
+Result
+call(
+	Result (&_fun)(Args...),
+	Args... _args
 )
 {
-	sge::opengl::call(
-		glEnable,
-		_what
-	);
-
-	SGE_OPENGL_CHECK_STATE(
-		(
-			fcppt::format(
-				FCPPT_TEXT("glEnable %1% failed")
-			)
-			% _what
-		).str(),
-		sge::renderer::exception
-	)
+	return
+		_fun(
+			_args...
+		);
 }
+
+}
+}
+
+#endif
