05a0e5a8a3bc0f7df75fffc24028dd87dfc87a16
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-09-26 17:04:26 +0200

    Don't send repeated keys on keyboards in XI2

--- src/plugins/x11input/src/focus/object.cpp
+++ src/plugins/x11input/src/focus/object.cpp
@@ -49,6 +49,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <fcppt/signal/auto_connection_container.hpp>
 #include <fcppt/signal/object_impl.hpp>
 #include <fcppt/config/external_begin.hpp>
+#include <X11/extensions/XI2.h>
 #include <X11/extensions/XInput2.h>
 #include <functional>
 #include <fcppt/config/external_end.hpp>
