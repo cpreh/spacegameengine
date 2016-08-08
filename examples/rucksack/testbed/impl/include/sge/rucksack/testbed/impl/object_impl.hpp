1785a5a25b556598d1807a181ce0a07d04e162e5
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-08-08 11:05:44 +0200

    Move gui impl files

--- examples/rucksack/testbed/impl/include/sge/rucksack/testbed/impl/object_impl.hpp
+++ examples/rucksack/testbed/impl/include/sge/rucksack/testbed/impl/object_impl.hpp
@@ -18,8 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#ifndef SGE_RUCKSACK_IMPL_TESTBED_OBJECT_IMPL_HPP_INCLUDED
-#define SGE_RUCKSACK_IMPL_TESTBED_OBJECT_IMPL_HPP_INCLUDED
+#ifndef SGE_RUCKSACK_TESTBED_IMPL_OBJECT_IMPL_HPP_INCLUDED
+#define SGE_RUCKSACK_TESTBED_IMPL_OBJECT_IMPL_HPP_INCLUDED
 
 #include <sge/image/color/rgba8_format.hpp>
 #include <sge/image/color/any/object_fwd.hpp>
