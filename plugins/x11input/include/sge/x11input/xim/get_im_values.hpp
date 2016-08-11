a8c3a9f561a3fc7a369714a67a7634ad9938fc65
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-08-11 16:27:07 +0200

    Add getter for supported XIM styles

---
+++ plugins/x11input/include/sge/x11input/xim/get_im_values.hpp
@@ -0,0 +1,81 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2016 Carl Philipp Reh (carlphilippreh <at> gmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef SGE_X11INPUT_XIM_GET_IM_VALUES_HPP_INCLUDED
+#define SGE_X11INPUT_XIM_GET_IM_VALUES_HPP_INCLUDED
+
+#include <sge/input/exception.hpp>
+#include <sge/x11input/xim/method.hpp>
+#include <fcppt/from_std_string.hpp>
+#include <fcppt/text.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <X11/Xlib.h>
+#include <utility>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace sge
+{
+namespace x11input
+{
+namespace xim
+{
+
+template<
+	typename... Args
+>
+void
+get_im_values(
+	sge::x11input::xim::method const &_method,
+	Args &&... _args
+)
+{
+	char const *const result{
+		::XGetIMValues(
+			_method.get(),
+			std::forward<
+				Args
+			>(
+				_args
+			)...,
+			NULL
+		)
+	};
+
+	if(
+		result
+		!=
+		nullptr
+	)
+		throw
+			sge::input::exception{
+				FCPPT_TEXT("XGetIMValues failed with ")
+				+
+				fcppt::from_std_string(
+					result
+				)
+			};
+}
+
+}
+}
+}
+
+#endif
---
+++ plugins/x11input/include/sge/x11input/xim/supported_styles_unique_ptr.hpp
@@ -0,0 +1,49 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2016 Carl Philipp Reh (carlphilippreh <at> gmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef SGE_X11INPUT_XIM_SUPPORTED_STYLES_UNIQUE_PTR_HPP_INCLUDED
+#define SGE_X11INPUT_XIM_SUPPORTED_STYLES_UNIQUE_PTR_HPP_INCLUDED
+
+#include <awl/backends/x11/deleter.hpp>
+#include <fcppt/unique_ptr_impl.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <X11/Xlib.h>
+#include <fcppt/config/external_end.hpp>
+
+
+namespace sge
+{
+namespace x11input
+{
+namespace xim
+{
+
+typedef
+fcppt::unique_ptr<
+	XIMStyles,
+	awl::backends::x11::deleter
+>
+supported_styles_unique_ptr;
+
+}
+}
+}
+
+#endif
