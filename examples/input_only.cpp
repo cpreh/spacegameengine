d67c048cb6d62e2568809fd433eab6d2fc9adb69
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-08-12 15:55:43 +0200

    Add input only example

---
+++ examples/input_only.cpp
@@ -0,0 +1,108 @@
+/*
+spacegameengine is a portable easy to use game engine written in C++.
+Copyright (C) 2006-2016 Carl Philipp Reh (carlphilippreh <at> gmail.com)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include <sge/systems/cursor_option_field.hpp>
+#include <sge/systems/input.hpp>
+#include <sge/systems/instance.hpp>
+#include <sge/systems/make_list.hpp>
+#include <sge/systems/original_window.hpp>
+#include <sge/systems/window.hpp>
+#include <sge/systems/window_source.hpp>
+#include <sge/systems/with_input.hpp>
+#include <sge/systems/with_window.hpp>
+#include <sge/window/system.hpp>
+#include <sge/window/title.hpp>
+#include <awl/show_error.hpp>
+#include <awl/show_error_narrow.hpp>
+#include <awl/main/exit_code.hpp>
+#include <awl/main/exit_failure.hpp>
+#include <awl/main/function_context.hpp>
+#include <fcppt/exception.hpp>
+#include <fcppt/text.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <boost/mpl/vector/vector10.hpp>
+#include <example_main.hpp>
+#include <exception>
+#include <fcppt/config/external_end.hpp>
+
+
+awl::main::exit_code const
+example_main(
+	awl::main::function_context const &
+)
+try
+{
+	sge::systems::instance<
+		boost::mpl::vector2<
+			sge::systems::with_window,
+			sge::systems::with_input<
+				boost::mpl::vector0<>
+			>
+		>
+	> const sys(
+		sge::systems::make_list
+		(
+			sge::systems::window{
+				sge::systems::window_source{
+					sge::systems::original_window{
+						sge::window::title{
+							FCPPT_TEXT("sge input-only example")
+						}
+					}
+				}
+			}
+		)
+		(
+			sge::systems::input(
+				sge::systems::cursor_option_field::null()
+			)
+		)
+	);
+
+	while(
+		sys.window_system().next()
+	)
+		;
+
+	return
+		sys.window_system().exit_code();
+}
+catch(
+	fcppt::exception const &_exception
+)
+{
+	awl::show_error(
+		_exception.string()
+	);
+
+	return
+		awl::main::exit_failure();
+}
+catch(
+	std::exception const &_exception
+)
+{
+	awl::show_error_narrow(
+		_exception.what()
+	);
+
+	return
+		awl::main::exit_failure();
+}
