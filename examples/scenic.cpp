196a1abedf3e39515a5d058e2e0e2e9c99fecbec
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-02-04 16:07:18 +0100

    Use fcppt::options in scenic

--- examples/scenic.cpp
+++ examples/scenic.cpp
@@ -88,21 +88,51 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <awl/main/exit_failure.hpp>
 #include <awl/main/exit_success.hpp>
 #include <awl/main/function_context.hpp>
+#include <fcppt/args_from_second.hpp>
 #include <fcppt/exception.hpp>
-#include <fcppt/from_std_string.hpp>
+#include <fcppt/literal.hpp>
 #include <fcppt/make_unique_ptr.hpp>
+#include <fcppt/string.hpp>
+#include <fcppt/strong_typedef_output.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/unique_ptr_impl.hpp>
 #include <fcppt/unique_ptr_to_base.hpp>
+#include <fcppt/either/match.hpp>
 #include <fcppt/io/cerr.hpp>
+#include <fcppt/io/cout.hpp>
 #include <fcppt/math/matrix/output.hpp>
 #include <fcppt/math/vector/output.hpp>
+#include <fcppt/optional/make.hpp>
+#include <fcppt/optional/map.hpp>
+#include <fcppt/optional/maybe.hpp>
 #include <fcppt/optional/maybe_void.hpp>
 #include <fcppt/optional/object_impl.hpp>
+#include <fcppt/options/apply.hpp>
+#include <fcppt/options/argument.hpp>
+#include <fcppt/options/default_help_switch.hpp>
+#include <fcppt/options/error.hpp>
+#include <fcppt/options/help_text.hpp>
+#include <fcppt/options/make_default_value.hpp>
+#include <fcppt/options/make_optional.hpp>
+#include <fcppt/options/no_default_value.hpp>
+#include <fcppt/options/long_name.hpp>
+#include <fcppt/options/option.hpp>
+#include <fcppt/options/optional_help_text.hpp>
+#include <fcppt/options/optional_short_name.hpp>
+#include <fcppt/options/parse_help.hpp>
+#include <fcppt/options/result.hpp>
+#include <fcppt/options/result_of.hpp>
+#include <fcppt/options/switch.hpp>
+#include <fcppt/record/element.hpp>
+#include <fcppt/record/get.hpp>
+#include <fcppt/record/make_label.hpp>
+#include <fcppt/record/permute.hpp>
+#include <fcppt/record/variadic.hpp>
 #include <fcppt/signal/auto_connection.hpp>
 #include <fcppt/signal/auto_connection.hpp>
+#include <fcppt/variant/match.hpp>
+#include <fcppt/variant/output.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <boost/program_options.hpp>
 #include <boost/mpl/vector/vector10.hpp>
 #include <chrono>
 #include <example_main.hpp>
@@ -113,77 +143,82 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <fcppt/config/external_end.hpp>
 
 
-awl::main::exit_code const
-example_main(
-	awl::main::function_context const &_context
-)
-try
+namespace
 {
-	std::string scene_name_narrow;
-	std::string record_to_file;
-	std::string track_from_file;
-	sge::camera::update_duration::rep exporter_interval;
 
-	boost::program_options::options_description allowed_options_description("Allowed options");
-	allowed_options_description.add_options()
-		(
-			"help",
-			"produce help message")
-		(
-			"record-to-file",
-			boost::program_options::value<std::string>(
-				&record_to_file),
-			"Use a first-person camera and record the keyframes to this file (use recording-interval to control the keyframe interval)")
-		(
-			"recording-interval",
-			boost::program_options::value<sge::camera::update_duration::rep>(
-				&exporter_interval)->default_value(
-					static_cast<sge::camera::update_duration::rep>(
-						0.25f)),
-			"Take a keyframe each recording-interval seconds (only makes sense together with record-to-file)")
-		(
-			"track-from-file",
-			boost::program_options::value<std::string>(
-				&track_from_file),
-			"Use a tracking camera and load keyframes from this file")
-		(
-			"scene-name",
-			boost::program_options::value<std::string>(
-				&scene_name_narrow)->required(),
-			"Scene name (denotes a path below media/scenes)")
-		(
-			"ffp",
-			"Use an ffp context instead of a shader context");
+FCPPT_RECORD_MAKE_LABEL(
+	record_to_file_label
+);
 
-	boost::program_options::variables_map compiled_options;
-	boost::program_options::store(
-		boost::program_options::parse_command_line(
-			_context.argc(),
-			_context.argv(),
-			allowed_options_description),
-		compiled_options);
+FCPPT_RECORD_MAKE_LABEL(
+	track_from_file_label
+);
 
-	if(compiled_options.count("help"))
-	{
-		std::cout << allowed_options_description << "\n";
-		return
-			awl::main::exit_success();
-	}
+FCPPT_RECORD_MAKE_LABEL(
+	recording_interval_label
+);
 
-	boost::program_options::notify(
-		compiled_options);
+FCPPT_RECORD_MAKE_LABEL(
+	ffp_label
+);
 
-	if(compiled_options.count("record-to-file") && compiled_options.count("track-from-file"))
+FCPPT_RECORD_MAKE_LABEL(
+	scene_name_label
+);
+
+typedef
+fcppt::record::variadic<
+	fcppt::record::element<
+		record_to_file_label,
+		fcppt::optional::object<
+			fcppt::string
+		>
+	>,
+	fcppt::record::element<
+		track_from_file_label,
+		fcppt::optional::object<
+			fcppt::string
+		>
+	>,
+	fcppt::record::element<
+		recording_interval_label,
+		sge::camera::update_duration::rep
+	>,
+	fcppt::record::element<
+		ffp_label,
+		bool
+	>,
+	fcppt::record::element<
+		scene_name_label,
+		fcppt::string
+	>
+>
+options_record;
+
+awl::main::exit_code
+main_program(
+	options_record const &_options
+)
+{
+	if(
+		fcppt::record::get<
+			record_to_file_label
+		>(
+			_options
+		).has_value()
+		&&
+		fcppt::record::get<
+			track_from_file_label
+		>(
+			_options
+		).has_value()
+	)
 	{
 		fcppt::io::cerr() <<
 			FCPPT_TEXT("You specified to record a file and use the tracking camera, that doesn't make sense.\n");
 		return
 			awl::main::exit_failure();
 	}
-
-	fcppt::string const scene_name(
-		fcppt::from_std_string(
-			scene_name_narrow));
 
 	sge::window::dim const window_dim(
 		1024u,
@@ -254,55 +289,72 @@ try
 
 	fcppt::signal::auto_connection const escape_connection(
 		sge::systems::quit_on_escape(
-			sys));
+			sys
+		)
+	);
 
-//#define SGE_EXAMPLE_SCENIC_CAPTURE
-
-	fcppt::unique_ptr<sge::camera::base> const camera(
-		compiled_options.count("track-from-file")
-		?
-			fcppt::unique_ptr_to_base<
-				sge::camera::base
+	fcppt::unique_ptr<
+		sge::camera::base
+	> const camera(
+		fcppt::optional::maybe(
+			fcppt::record::get<
+				track_from_file_label
 			>(
-				fcppt::make_unique_ptr<
-					sge::camera::tracking::object
-				>(
-					sge::camera::optional_projection_matrix(),
-					sge::camera::tracking::json::keyframes_from_json(
-						sge::parse::json::parse_file_exn(
-							boost::filesystem::path(
-								track_from_file
+				_options
+			),
+			[
+				&sys
+			]{
+				return
+					fcppt::unique_ptr_to_base<
+						sge::camera::base
+					>(
+						fcppt::make_unique_ptr<
+							sge::camera::first_person::object
+						>(
+							sge::camera::first_person::parameters(
+								sys.keyboard_collector(),
+								sys.mouse_collector(),
+								sge::camera::is_active(
+									true
+								),
+								sge::camera::first_person::movement_speed(
+									4.0f
+								),
+								sge::camera::coordinate_system::identity()
 							)
-						).array()
-					),
-					sge::camera::tracking::is_looping(
-						true
-					),
-					sge::camera::is_active(
-						true
-					)
-				)
+						)
+					);
+			},
+			[](
+				fcppt::string const &_track_from_file
 			)
-		:
-			fcppt::unique_ptr_to_base<
-				sge::camera::base
-			>(
-				fcppt::make_unique_ptr<
-					sge::camera::first_person::object
-				>(
-					sge::camera::first_person::parameters(
-						sys.keyboard_collector(),
-						sys.mouse_collector(),
-						sge::camera::is_active(
-							true
-						),
-						sge::camera::first_person::movement_speed(
-							4.0f
-						),
-						sge::camera::coordinate_system::identity()
-					)
-				)
-			)
+			{
+				return
+					fcppt::unique_ptr_to_base<
+						sge::camera::base
+					>(
+						fcppt::make_unique_ptr<
+							sge::camera::tracking::object
+						>(
+							sge::camera::optional_projection_matrix(),
+							sge::camera::tracking::json::keyframes_from_json(
+								sge::parse::json::parse_file_exn(
+									boost::filesystem::path(
+										_track_from_file
+									)
+								).array()
+							),
+							sge::camera::tracking::is_looping(
+								true
+							),
+							sge::camera::is_active(
+								true
+							)
+						)
+					);
+			}
+		)
 	);
 
 	typedef
@@ -318,31 +370,50 @@ try
 	optional_exporter_unique_ptr;
 
 	optional_exporter_unique_ptr const exporter(
-		compiled_options.count("record-to-file")
-		?
-			optional_exporter_unique_ptr(
-				fcppt::make_unique_ptr<
-					sge::camera::tracking::json::interval_exporter
-				>(
-					*camera,
-					sge::camera::update_duration(
-						exporter_interval
-					),
-					boost::filesystem::path(
-						record_to_file
-					)
-				)
+		fcppt::optional::map(
+			fcppt::record::get<
+				record_to_file_label
+			>(
+				_options
+			),
+			[
+				&_options,
+				&camera
+			](
+				fcppt::string const &_record_to_file
 			)
-		:
-			optional_exporter_unique_ptr()
+			{
+				return
+					fcppt::make_unique_ptr<
+						sge::camera::tracking::json::interval_exporter
+					>(
+						*camera,
+						sge::camera::update_duration{
+							fcppt::record::get<
+								recording_interval_label
+							>(
+								_options
+							)
+						},
+						boost::filesystem::path(
+							_record_to_file
+						)
+					);
+			}
+		)
 	);
 
 	sge::scenic::scene::manager scene_manager(
 		sys.renderer_device_core(),
 		sys.image_system(),
 		sge::scenic::scene::prefer_cg_context(
-			!compiled_options.count(
-				"ffp")));
+			!fcppt::record::get<
+				ffp_label
+			>(
+				_options
+			)
+		)
+	);
 
 	sge::scenic::scene::object test_scene(
 		sys.log_context(),
@@ -350,7 +421,17 @@ try
 		sys.viewport_manager(),
 		*camera,
 		sge::scenic::scene::from_blender_file(
-			sge::config::media_path() / FCPPT_TEXT("scenes") / scene_name / FCPPT_TEXT("description.json")
+			sge::config::media_path()
+			/
+			FCPPT_TEXT("scenes")
+			/
+			fcppt::record::get<
+				scene_name_label
+			>(
+				_options
+			)
+			/
+			FCPPT_TEXT("description.json")
 		)
 	);
 
@@ -366,24 +447,31 @@ try
 		)
 	);
 
-	sge::scenic::grid::object simple_grid_xz(
+	sge::scenic::grid::object simple_grid_xz{
 		sys.renderer_device_ffp(),
 		*camera,
 		sge::scenic::grid::orientation::xz,
-		sge::scenic::grid::rect(
-			sge::scenic::grid::rect::vector(
+		sge::scenic::grid::rect{
+			sge::scenic::grid::rect::vector{
 				-10.0f,
-				-10.0f),
-			sge::scenic::grid::rect::dim(
+				-10.0f
+			},
+			sge::scenic::grid::rect::dim{
 				20.0f,
-				20.0f)),
-			sge::scenic::grid::spacing(
-				sge::scenic::grid::dim(
-					1.0f,
-					1.0f)),
-		sge::scenic::grid::distance_to_origin(
-			0.0f),
-		sge::image::color::predef::white());
+				20.0f
+			}
+		},
+		sge::scenic::grid::spacing{
+			sge::scenic::grid::dim{
+				1.0f,
+				1.0f
+			}
+		},
+		sge::scenic::grid::distance_to_origin{
+			0.0f
+		},
+		sge::image::color::predef::white()
+	};
 
 	/*
 	sge::scenic::sky::dome::object skydome(
@@ -401,23 +489,31 @@ try
 			0.0f));
 	*/
 
-	sge::graph::object graph(
-		sge::graph::position(
-			sge::renderer::vector2(
+	sge::graph::object graph{
+		sge::graph::position{
+			sge::renderer::vector2{
 				0.0f,
-				0.0f)),
-		sge::renderer::dim2(
+				0.0f
+			}
+		},
+		sge::renderer::dim2{
 			256u,
-			64u),
+			64u
+		},
 		sys.renderer_device_ffp(),
-		sge::graph::baseline(
-			1.0f/30.0f),
-		sge::graph::optional_axis_constraint(
-			sge::graph::axis_constraint(
+		sge::graph::baseline{
+			1.0f
+			/
+			30.0f
+		},
+		sge::graph::optional_axis_constraint{
+			sge::graph::axis_constraint{
 				0.0f,
-				0.1f)),
+				0.1f
+			}
+		},
 		sge::graph::color_schemes::default_()
-	);
+	};
 
 	/*
 	camera.update_coordinate_system(
@@ -437,11 +533,17 @@ try
 			)
 		);
 
-		dynamic_cast<sge::camera::is_dynamic &>(*camera).update(
-			difference_since_last_frame);
+		dynamic_cast<
+			sge::camera::is_dynamic &
+		>(
+			*camera
+		).update(
+			difference_since_last_frame
+		);
 
 		graph.push(
-			difference_since_last_frame.count());
+			difference_since_last_frame.count()
+		);
 
 		fcppt::optional::maybe_void(
 			exporter,
@@ -453,16 +555,20 @@ try
 			}
 		);
 
-		sge::renderer::context::scoped_ffp const scoped_block(
+		sge::renderer::context::scoped_ffp const scoped_block{
 			sys.renderer_device_ffp(),
-			sys.renderer_device_ffp().onscreen_target());
+			sys.renderer_device_ffp().onscreen_target()
+		};
 
 		scoped_block.get().clear(
 			sge::renderer::clear::parameters()
 				.back_buffer(
-					sge::image::color::predef::black())
+					sge::image::color::predef::black()
+				)
 				.depth_buffer(
-					1.0f));
+					1.0f
+				)
+		);
 
 		/*
 		skydome.render(
@@ -470,28 +576,208 @@ try
 		*/
 
 		{
-			sge::scenic::render_context::base_unique_ptr wrapped_context(
+			sge::scenic::render_context::base_unique_ptr const wrapped_context{
 				scene_manager.create_render_context(
-					scoped_block.get()));
+					scoped_block.get()
+				)
+			};
 
 			test_scene.render(
-				*wrapped_context);
+				*wrapped_context
+			);
 		}
 
 		simple_grid_xz.render(
 			scoped_block.get(),
-			sge::scenic::grid::depth_test(
-				true));
+			sge::scenic::grid::depth_test{
+				true
+			}
+		);
 
 		graph.render(
-			scoped_block.get());
+			scoped_block.get()
+		);
 	}
 
 	return
 		sys.window_system().exit_code();
 }
+
+}
+
+awl::main::exit_code const
+example_main(
+	awl::main::function_context const &_function_context
+)
+try
+{
+	auto const argument_parser{
+		fcppt::options::apply(
+			fcppt::options::make_optional(
+				fcppt::options::option<
+					record_to_file_label,
+					fcppt::string
+				>{
+					fcppt::options::optional_short_name{},
+					fcppt::options::long_name{
+						FCPPT_TEXT("record-to-file")
+					},
+					fcppt::options::no_default_value<
+						fcppt::string
+					>(),
+					fcppt::options::optional_help_text{
+						fcppt::options::help_text{
+							FCPPT_TEXT("Use a first-person camera and record the keyframes to this file (use recording-interval to control the keyframe interval)")
+						}
+					}
+				}
+			),
+			fcppt::options::option<
+				recording_interval_label,
+				sge::camera::update_duration::rep
+			>{
+				fcppt::options::optional_short_name{},
+				fcppt::options::long_name{
+					FCPPT_TEXT("recording-interval")
+				},
+				fcppt::options::make_default_value(
+					fcppt::optional::make(
+						sge::camera::update_duration::rep{
+							fcppt::literal<
+								sge::renderer::scalar
+							>(
+								0.25f
+							)
+						}
+					)
+				),
+				fcppt::options::optional_help_text{
+					fcppt::options::help_text{
+						FCPPT_TEXT("Take a keyframe each recording-interval seconds (only makes sense together with record-to-file)")
+					}
+				}
+			},
+			fcppt::options::make_optional(
+				fcppt::options::option<
+					track_from_file_label,
+					fcppt::string
+				>{
+					fcppt::options::optional_short_name{},
+					fcppt::options::long_name{
+						FCPPT_TEXT("track-from-file")
+					},
+					fcppt::options::no_default_value<
+						fcppt::string
+					>(),
+					fcppt::options::optional_help_text{
+						fcppt::options::help_text{
+							FCPPT_TEXT("Use a tracking camera and load keyframes from this file")
+						}
+					}
+				}
+			),
+			fcppt::options::switch_<
+				ffp_label
+			>{
+				fcppt::options::optional_short_name{},
+				fcppt::options::long_name{
+					FCPPT_TEXT("ffp")
+				},
+				fcppt::options::optional_help_text{
+					fcppt::options::help_text{
+						FCPPT_TEXT("Use an ffp context instead of a shader context")
+					}
+				}
+			},
+			fcppt::options::argument<
+				scene_name_label,
+				fcppt::string
+			>{
+				fcppt::options::long_name{
+					FCPPT_TEXT("scene-name")
+				},
+				fcppt::options::optional_help_text{
+					fcppt::options::help_text{
+						FCPPT_TEXT("Scene name (denotes a path below media/scenes)")
+					}
+				}
+			}
+		)
+	};
+
+	typedef
+	fcppt::options::result_of<
+		decltype(
+			argument_parser
+		)
+	>
+	result_type;
+
+	return
+		fcppt::variant::match(
+			fcppt::options::parse_help(
+				fcppt::options::default_help_switch(),
+				argument_parser,
+				fcppt::args_from_second(
+					_function_context.argc(),
+					_function_context.argv()
+				)
+			),
+			[](
+				fcppt::options::result<
+					result_type
+				> const _result
+			)
+			{
+				return
+					fcppt::either::match(
+						_result,
+						[](
+							fcppt::options::error const &_error
+						)
+						{
+							fcppt::io::cerr()
+								<<
+								_error
+								<<
+								FCPPT_TEXT('\n');
+
+							return
+								awl::main::exit_failure();
+						},
+						[](
+							result_type const &_options
+						)
+						{
+							return
+								main_program(
+									fcppt::record::permute<
+										options_record
+									>(
+										_options
+									)
+								);
+						}
+					);
+			},
+			[](
+				fcppt::options::help_text const &_help_text
+			)
+			{
+				fcppt::io::cout()
+					<<
+					_help_text
+					<<
+					FCPPT_TEXT('\n');
+
+				return
+					awl::main::exit_success();
+			}
+		);
+}
 catch(
-	fcppt::exception const &_error)
+	fcppt::exception const &_error
+)
 {
 	fcppt::io::cerr()
 		<< _error.string()
@@ -501,7 +787,8 @@ catch(
 		awl::main::exit_failure();
 }
 catch(
-	std::exception const &_error)
+	std::exception const &_error
+)
 {
 	std::cerr
 		<< _error.what()
