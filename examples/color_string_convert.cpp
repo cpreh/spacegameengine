476983eeda34651f33c352b5acb1745d18bcfe5e
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-05-01 10:31:39 +0200

    Use fcppt.options in color_string_convert

--- examples/color_string_convert.cpp
+++ examples/color_string_convert.cpp
@@ -21,33 +21,113 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/image/exception.hpp>
 #include <sge/image/color/any/from_string.hpp>
 #include <sge/image/color/any/print.hpp>
+#include <fcppt/args_char.hpp>
+#include <fcppt/args_from_second.hpp>
+#include <fcppt/main.hpp>
+#include <fcppt/string.hpp>
+#include <fcppt/strong_typedef_output.hpp>
 #include <fcppt/text.hpp>
+#include <fcppt/either/match.hpp>
+#include <fcppt/io/cerr.hpp>
 #include <fcppt/io/cout.hpp>
+#include <fcppt/options/argument.hpp>
+#include <fcppt/options/error.hpp>
+#include <fcppt/options/long_name.hpp>
+#include <fcppt/options/optional_help_text.hpp>
+#include <fcppt/options/parse.hpp>
+#include <fcppt/options/result_of.hpp>
+#include <fcppt/record/get.hpp>
+#include <fcppt/record/make_label.hpp>
+#include <fcppt/variant/output.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <cstddef>
-#include <ostream>
 #include <fcppt/config/external_end.hpp>
 
 
-// TODO: Use fcppt.options
 int
-main(
-	int const argc,
-	char *argv[])
+FCPPT_MAIN(
+	int argc,
+	fcppt::args_char **argv
+)
 try
 {
-	if(argc != 2)
-	{
-		fcppt::io::cout() << FCPPT_TEXT("Usage: ") << argv[0] << FCPPT_TEXT(" <color-string>\n");
-		return EXIT_FAILURE;
-	}
-	// FIXME
-	fcppt::io::cout() << sge::image::color::any::from_string(argv[1]) << FCPPT_TEXT("\n");
-	return EXIT_SUCCESS;
+	FCPPT_RECORD_MAKE_LABEL(
+		string_label
+	);
+
+	typedef
+	fcppt::options::argument<
+		string_label,
+		fcppt::string
+	>
+	argument;
+
+	typedef
+	fcppt::options::result_of<
+		argument
+	>
+	result_type;
+
+	argument const parser{
+		fcppt::options::long_name{
+			FCPPT_TEXT("color-string")
+		},
+		fcppt::options::optional_help_text{}
+	};
+
+	return
+		fcppt::either::match(
+			fcppt::options::parse(
+				parser,
+				fcppt::args_from_second(
+					argc,
+					argv
+				)
+			),
+			[](
+				fcppt::options::error const &_error
+			)
+			{
+				fcppt::io::cerr()
+					<<
+					_error
+					<<
+					FCPPT_TEXT('\n');
+
+				return
+					EXIT_FAILURE;
+			},
+			[](
+				result_type const &_result
+			)
+			{
+				fcppt::io::cout()
+					<<
+					sge::image::color::any::from_string(
+						fcppt::record::get<
+							string_label
+						>(
+							_result
+						)
+					)
+					<<
+					FCPPT_TEXT('\n');
+
+				return
+					EXIT_SUCCESS;
+			}
+		);
 }
 catch(
-	 sge::image::exception const &e)
+	 sge::image::exception const &_error
+)
 {
-	 fcppt::io::cout() << e.string() << FCPPT_TEXT("\n");
-	 return EXIT_FAILURE;
+	 fcppt::io::cerr()
+	 	<<
+		_error.string()
+		<<
+		FCPPT_TEXT('\n');
+
+	 return
+	 	EXIT_FAILURE;
 }
