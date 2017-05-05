a1e3934877ec110998c28672fbc61a3c8a3a3a36
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-05-05 18:08:57 +0200

    Improve create_exn error message.

--- libs/image2d/src/image2d/create_exn.cpp
+++ libs/image2d/src/image2d/create_exn.cpp
@@ -19,11 +19,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 #include <sge/image/exception.hpp>
+#include <sge/image/color/format_to_string.hpp>
 #include <sge/image2d/create_exn.hpp>
 #include <sge/image2d/file.hpp>
 #include <sge/image2d/file_unique_ptr.hpp>
 #include <sge/image2d/system.hpp>
 #include <sge/image2d/view/const_object_fwd.hpp>
+#include <sge/image2d/view/format.hpp>
 #include <sge/media/extension_fwd.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/optional/to_exception.hpp>
@@ -43,13 +45,24 @@ sge::image2d::create_exn(
 				_extension
 			),
 			[
+				&_view,
 				&_extension
 			]{
 				return
 					sge::image::exception(
-						FCPPT_TEXT("create_exn failed with extension ")
+						FCPPT_TEXT("No image2d system can create an image with extension ")
 						+
 						_extension.get()
+						+
+						FCPPT_TEXT(" and type ")
+						+
+						sge::image::color::format_to_string(
+							sge::image2d::view::format(
+								_view
+							)
+						)
+						+
+						FCPPT_TEXT('!')
 					);
 			}
 		);
