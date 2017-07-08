870d3a2f608caad617823b2a27c5eecdcb4870e8
Carl Philipp Reh <carlphilippreh@gmail.com>
2017-07-08 11:00:13 +0200

    Fix for fcppt

--- libs/parsejson/include/sge/parse/json/convert_from_impl.hpp
+++ libs/parsejson/include/sge/parse/json/convert_from_impl.hpp
@@ -270,11 +270,11 @@ struct convert_from_impl<
 							Result::value_type
 						>(
 							std::get<
-								_index.row
+								_index.row()
 							>(
 								inner
 							).get()[
-								_index.column
+								_index.column()
 							]
 						);
 				}
