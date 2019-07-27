6ce83bb195cbce64c8a985719a1a4d23c045cd13
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-07-27 10:30:52 +0200

    Add opengl::sdl::make_current

--- plugins/opengl/src/sdl/make_current.cpp
+++ plugins/opengl/src/sdl/make_current.cpp
@@ -18,76 +18,31 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
-#include <sge/opengl/backend/current.hpp>
-#include <sge/opengl/backend/fun_ptr.hpp>
-#include <sge/opengl/sdl/current.hpp>
 #include <sge/opengl/sdl/make_current.hpp>
-#include <sge/renderer/display_mode/vsync.hpp>
+#include <sge/renderer/exception.hpp>
 #include <awl/backends/sdl/window/object.hpp>
+#include <fcppt/text.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <SDL_video.h>
-#include <string>
 #include <fcppt/config/external_end.hpp>
 
 
-sge::opengl::sdl::current::current(
+void
+sge::opengl::sdl::make_current(
 	awl::backends::sdl::window::object &_window,
 	SDL_GLContext const _context
 )
-:
-	sge::opengl::backend::current{},
-	window_{
-		_window
-	},
-	context_{
-		_context
-	}
 {
-	sge::opengl::sdl::make_current(
-		this->window_,
-		this->context_
-	);
-}
-
-sge::opengl::sdl::current::~current()
-{
-	sge::opengl::sdl::make_current(
-		this->window_,
-		nullptr
-	);
-}
-
-sge::opengl::backend::fun_ptr
-sge::opengl::sdl::current::load_function(
-	std::string const &_function
-) const
-{
-	return
-		reinterpret_cast<
-			sge::opengl::backend::fun_ptr
-		>(
-			SDL_GL_GetProcAddress(
-				_function.c_str()
-			)
-		);
-}
-
-void
-sge::opengl::sdl::current::begin_rendering()
-{
-}
-
-void
-sge::opengl::sdl::current::end_rendering()
-{
-	SDL_GL_SwapWindow(
-		&this->window_.get()
-	);
-}
-
-void
-sge::opengl::sdl::current::vsync(
-	sge::renderer::display_mode::vsync const _vsync
-)
-{
+	if(
+		SDL_GL_MakeCurrent(
+			&_window.get(),
+			_context
+		)
+		!=
+		0
+	)
+		throw
+			sge::renderer::exception{
+				FCPPT_TEXT("SDL_GL_MakeCurrent failed")
+			};
 }
