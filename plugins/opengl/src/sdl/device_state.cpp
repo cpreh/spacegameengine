33f9c1405b06ed46031d678faa5fec59e7cdf612
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-07-29 13:56:27 +0200

    Set fullscreen in SDL

--- plugins/opengl/src/sdl/device_state.cpp
+++ plugins/opengl/src/sdl/device_state.cpp
@@ -21,21 +21,28 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sge/opengl/platform/device_state.hpp>
 #include <sge/opengl/sdl/device_state.hpp>
 #include <sge/opengl/sdl/get_window_display_mode.hpp>
+#include <sge/renderer/exception.hpp>
 #include <sge/renderer/screen_size.hpp>
+#include <sge/renderer/display_mode/fullscreen.hpp>
+#include <sge/renderer/display_mode/fullscreen_desktop.hpp>
 #include <sge/renderer/display_mode/object.hpp>
 #include <sge/renderer/display_mode/optional_dimensions.hpp>
-#include <sge/renderer/display_mode/optional_fullscreen_fwd.hpp>
+#include <sge/renderer/display_mode/optional_fullscreen.hpp>
 #include <sge/renderer/display_mode/optional_object.hpp>
 #include <sge/renderer/display_mode/pixel_size.hpp>
 #include <sge/renderer/display_mode/refresh_rate.hpp>
 #include <sge/window/object.hpp>
 #include <awl/backends/sdl/window/object.hpp>
+#include <fcppt/text.hpp>
 #include <fcppt/cast/dynamic_exn.hpp>
 #include <fcppt/cast/to_unsigned.hpp>
 #include <fcppt/optional/make_if.hpp>
 #include <fcppt/optional/map.hpp>
+#include <fcppt/optional/maybe.hpp>
+#include <fcppt/variant/match.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <SDL_video.h>
+#include <cstdint>
 #include <fcppt/config/external_end.hpp>
 
 
@@ -109,7 +116,71 @@ sge::opengl::sdl::device_state::display_mode() const
 
 void
 sge::opengl::sdl::device_state::fullscreen(
-	sge::renderer::display_mode::optional_fullscreen const &_fullscreen
+	sge::renderer::display_mode::optional_fullscreen const &_opt_fullscreen
 )
 {
+	auto const set_fullscreen(
+		[
+			this
+		](
+			std::uint32_t const _value
+		)
+		{
+			if(
+				SDL_SetWindowFullscreen(
+					&this->window_.get(),
+					_value
+				)
+				==
+				-1
+			)
+				throw
+					sge::renderer::exception{
+						FCPPT_TEXT("SDL_SetWindowFullscreen failed!")
+					};
+
+		}
+	);
+
+	fcppt::optional::maybe(
+		_opt_fullscreen,
+		[
+			&set_fullscreen
+		]{
+			set_fullscreen(
+				0
+			);
+		},
+		[
+			&set_fullscreen
+		](
+			sge::renderer::display_mode::fullscreen const &_fullscreen
+		)
+		{
+			fcppt::variant::match(
+				_fullscreen,
+				[
+					&set_fullscreen
+				](
+					sge::renderer::display_mode::fullscreen_desktop const &
+				)
+				{
+					set_fullscreen(
+						SDL_WINDOW_FULLSCREEN_DESKTOP
+					);
+				},
+				[
+					&set_fullscreen
+				](
+					sge::renderer::display_mode::object const &_display_mode
+				)
+				{
+					// TODO: Set display_mode
+					set_fullscreen(
+						SDL_WINDOW_FULLSCREEN
+					);
+				}
+			);
+		}
+	);
 }
