4eb797ba3fb11208a3dbafa04aa83d449b373441
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-07-29 18:50:08 +0200

    Fix FindBullet.cmake

--- cmake/modules/private/FindBULLET.cmake
+++ cmake/modules/private/FindBULLET.cmake
@@ -4,22 +4,26 @@ pkg_check_modules(
 	bullet
 )
 
-get_target_property(
-	INCLUDE_DIRS
-	PkgConfig::BULLET
-	INTERFACE_INCLUDE_DIRECTORIES
+if(
+	BULLET_FOUND
 )
+	get_target_property(
+		INCLUDE_DIRS
+		PkgConfig::BULLET
+		INTERFACE_INCLUDE_DIRECTORIES
+	)
 
-set_target_properties(
-	PkgConfig::BULLET
-	PROPERTIES
-	INTERFACE_INCLUDE_DIRECTORIES
-	""
-)
+	set_target_properties(
+		PkgConfig::BULLET
+		PROPERTIES
+		INTERFACE_INCLUDE_DIRECTORIES
+		""
+	)
 
-target_include_directories(
-	PkgConfig::BULLET
-	SYSTEM
-	INTERFACE
-	${INCLUDE_DIRS}
-)
+	target_include_directories(
+		PkgConfig::BULLET
+		SYSTEM
+		INTERFACE
+		${INCLUDE_DIRS}
+	)
+endif()
