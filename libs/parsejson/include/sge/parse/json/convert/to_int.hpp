34975fb8935b7f37ada1b44fc8b016f376853d87
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-04-21 11:42:48 +0200

    Update for fcppt

--- libs/parsejson/include/sge/parse/json/convert/to_int.hpp
+++ libs/parsejson/include/sge/parse/json/convert/to_int.hpp
@@ -7,11 +7,14 @@
 #ifndef SGE_PARSE_JSON_CONVERT_TO_INT_HPP_INCLUDED
 #define SGE_PARSE_JSON_CONVERT_TO_INT_HPP_INCLUDED
 
+#include <sge/parse/json/exception.hpp>
 #include <sge/parse/json/get_exn.hpp>
 #include <sge/parse/json/int_type.hpp>
 #include <sge/parse/json/value.hpp>
 #include <fcppt/make_cref.hpp>
+#include <fcppt/text.hpp>
 #include <fcppt/cast/truncation_check.hpp>
+#include <fcppt/optional/to_exception.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <type_traits>
 #include <fcppt/config/external_end.hpp>
@@ -42,16 +45,24 @@ to_int(
 	);
 
 	return
-		fcppt::cast::truncation_check<
-			IntType
-		>(
-			sge::parse::json::get_exn<
-				sge::parse::json::int_type const
+		fcppt::optional::to_exception(
+			fcppt::cast::truncation_check<
+				IntType
 			>(
-				fcppt::make_cref(
-					_value
-				)
-			).get()
+				sge::parse::json::get_exn<
+					sge::parse::json::int_type const
+				>(
+					fcppt::make_cref(
+						_value
+					)
+				).get()
+			),
+			[]{
+				return
+					sge::parse::json::exception{
+						FCPPT_TEXT("convert::to_int: value too large")
+					};
+			}
 		);
 }
 
