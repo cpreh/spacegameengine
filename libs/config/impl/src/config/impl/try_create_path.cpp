42e35b6f57f71abe414746d6d39bbc0bc08a8a20
Carl Philipp Reh <carlphilippreh@gmail.com>
2021-07-19 10:33:24 +0200

    Ignore a warning in try_create_path

--- libs/config/impl/src/config/impl/try_create_path.cpp
+++ libs/config/impl/src/config/impl/try_create_path.cpp
@@ -7,8 +7,10 @@
 #include <sge/config/exception.hpp>
 #include <sge/config/impl/try_create_path.hpp>
 #include <fcppt/error_code_to_string.hpp>
+#include <fcppt/use.hpp>
 #include <fcppt/text.hpp>
 #include <fcppt/either/error_from_optional.hpp>
+#include <fcppt/either/no_error.hpp>
 #include <fcppt/either/to_exception.hpp>
 #include <fcppt/filesystem/create_directories_recursive.hpp>
 #include <fcppt/filesystem/path_to_string.hpp>
@@ -23,33 +25,39 @@ sge::config::impl::try_create_path(
 	std::filesystem::path const &_path
 )
 {
-	fcppt::either::to_exception(
-		fcppt::either::error_from_optional(
-			fcppt::filesystem::create_directories_recursive(
-				_path
+	fcppt::either::no_error const result{
+		fcppt::either::to_exception(
+			fcppt::either::error_from_optional(
+				fcppt::filesystem::create_directories_recursive(
+					_path
+				)
+			),
+			[
+				&_path
+			](
+				std::error_code const _error
 			)
-		),
-		[
-			&_path
-		](
-			std::error_code const _error
+			{
+				return
+					sge::config::exception{
+						FCPPT_TEXT("Failed to create ")
+						+
+						fcppt::filesystem::path_to_string(
+							_path
+						)
+						+
+						FCPPT_TEXT(". Reason: ")
+						+
+						fcppt::error_code_to_string(
+							_error
+						)
+					};
+			}
 		)
-		{
-			return
-				sge::config::exception{
-					FCPPT_TEXT("Failed to create ")
-					+
-					fcppt::filesystem::path_to_string(
-						_path
-					)
-					+
-					FCPPT_TEXT(". Reason: ")
-					+
-					fcppt::error_code_to_string(
-						_error
-					)
-				};
-		}
+	};
+
+	FCPPT_USE(
+		result
 	);
 
 	return
