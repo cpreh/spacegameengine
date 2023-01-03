6e4550bfd9c0e4b53f6f32de8f55073e0d3df4d7
Carl Philipp Reh <carlphilippreh@gmail.com>
2023-01-03 10:39:04 +0100

    Remove ASSERT_ERROR in systems

--- libs/systems/src/systems/detail/list.cpp
+++ libs/systems/src/systems/detail/list.cpp
@@ -3,13 +3,14 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <sge/systems/exception.hpp>
 #include <sge/systems/detail/any.hpp>
 #include <sge/systems/detail/any_list.hpp>
 #include <sge/systems/detail/any_map.hpp>
 #include <sge/systems/detail/list.hpp>
 #include <sge/systems/impl/make_any_key.hpp>
+#include <fcppt/text.hpp>
 #include <fcppt/algorithm/map.hpp>
-#include <fcppt/assert/error.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <utility>
 #include <fcppt/config/external_end.hpp>
@@ -20,7 +21,10 @@ sge::systems::detail::list::list(sge::systems::detail::any_list const &_elements
           [](sge::systems::detail::any const &_any)
           { return std::make_pair(sge::systems::impl::make_any_key(_any), _any); }))
 {
-  FCPPT_ASSERT_ERROR(_elements.size() == states_.size());
+  if (_elements.size() != states_.size())
+  {
+    throw sge::systems::exception{FCPPT_TEXT("Invalid list init!")};
+  }
 }
 
 sge::systems::detail::any_map const &sge::systems::detail::list::get() const { return states_; }
