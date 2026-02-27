56bf8a7b804e1622b382ae65a280ac0e5f521409
Carl Philipp Reh <carlphilippreh@gmail.com>
2026-02-27 21:55:39 +0100

    Fix clang-22 warning

--- plugins/wave/include/sge/wave/file.hpp
+++ plugins/wave/include/sge/wave/file.hpp
@@ -20,7 +20,8 @@
 #include <fcppt/reference_fwd.hpp>
 #include <fcppt/log/object_reference.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <ios>
+#include <iosfwd>
+#include <ios> // IWYU pragma: keep
 #include <fcppt/config/external_end.hpp>
 
 namespace sge::wave
